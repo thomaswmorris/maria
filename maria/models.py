8ad1b2f315f61e3a320a6170d040d4dc2e63ca97
Thomas Morris <tom@mbp2-3.local>
2023-03-16 23:27:04 -0400

    added fixed quantiles

--- maria/models.py
+++ maria/models.py
@@ -56,7 +56,7 @@ class LinearAngularModel(AtmosphericModel):
 
         self.min_ang_res = self.angular_waists / self.min_beam_res
 
-        self.weather.generate(self.pointing.unix, fixed_quantiles=self.site.fixed_quantiles)
+        self.weather.generate(time=self.pointing.unix, fixed_quantiles=self.site.fixed_quantiles)
 
         self.heights = self.site.altitude + self.layer_depths[:, None] * np.sin(self.pointing.el)[None, :]
 
