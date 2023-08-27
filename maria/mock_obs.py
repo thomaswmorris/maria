9ec97c99ca92a2c40face83caa637d8a5fda8803
Thomas Morris <thomasmorris@princeton.edu>
2023-08-26 22:57:58 -0400

    fix notebooks
    Former-commit-id: 6f1da5637b155cc6c27bfbea371a7194eae77a3e [formerly dfe31312e3acba0042960f6110534db1f87533d6] [formerly a830ff9ea0b41b7af897471bb0de9759af969bbd [formerly 1bcd68444b6e41453061f61c403d79b938a0c66f]]
    Former-commit-id: 833ef4ee0e4db72a24239880fd5686f8863da8e5 [formerly 957201f6568dfdc6ef9cc1791a90c49b67c7e0fb]
    Former-commit-id: 77217844c1daa986d90f64854afafa6a9ae251a3

--- maria/mock_obs.py
+++ maria/mock_obs.py
@@ -11,7 +11,7 @@ from . import get_array, get_site, get_pointing
 from . import utils
 
 class WeObserve:
-    def __init__(self, project, skymodel, array_name='AtLAST', pointing_name='DAISY-2deg_4ra_10.5dec_600s', site_name='APEX', verbose=True, cmb = False, **kwargs):
+    def __init__(self, project, skymodel, array_name='AtLAST', pointing_name='daisy_4ra_10.5dec_600s', site_name='APEX', verbose=True, cmb = False, **kwargs):
 
         self.verbose   = verbose
         self.file_name = skymodel
