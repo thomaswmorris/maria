165319f07477c9a377c9c3c38df5aa9c1327b829
Thomas Morris <thomasmorris@princeton.edu>
2023-10-17 15:43:18 -0400

    tests pass now
    Former-commit-id: e7f0c6e517674288c1cae2dcdcaf964b862e8e3a [formerly f1420a743c7825d0b730255a82f2e66eac16cc7b] [formerly 5b8b9cc27b0b5e946db5b6309aadb65901264c89 [formerly ea49e76f01893d19d73b61fcc1318a30ce64c9f4]]
    Former-commit-id: 9e1b170534ec19cc5f97f030eb06c78c33c686f2 [formerly 6bae12125a12015528697bb66fe57299251fa1b4]
    Former-commit-id: 998508bbfedc54cf9936caad47fb0f2444a82002

--- maria/utils/linalg.py
+++ maria/utils/linalg.py
@@ -1,6 +1,27 @@
 import numpy as np
 import scipy as sp
 
+def get_rotation_matrix_from_angle(a):
+    S = np.atleast_1d(a)[..., None, None] * np.array([[0., 1.], [-1., 0.]])
+    R = sp.linalg.expm(S)
+    return R.reshape(*np.shape(a), 2, 2)
+
+def optimize_area_minimizing_rotation_matrix(points):   
+    
+    def log_dimension_ratio(a):
+        trans_points = points @ get_rotation_matrix_from_angle(a).T
+        log_ratio = np.log(trans_points[:, 0].ptp() / trans_points[:, 1].ptp())
+        return log_ratio
+    
+    test_angles = np.linspace(0, np.pi, 64)[:-1]
+    test_ratios = list(map(log_dimension_ratio, test_angles))
+    
+    res = sp.optimize.minimize(lambda p: log_dimension_ratio(p[0]), 
+                               x0=[test_angles[np.argmin(test_ratios)]], 
+                               bounds=[(0, np.pi)], 
+                               method="Nelder-Mead")
+
+    return res
 
 def mprod(*M):
     if not len(M) > 0:
@@ -9,7 +30,6 @@ def mprod(*M):
     for M_ in M[1:]:
         res = np.matmul(res, M_)
     return res
-
 
 def fast_psd_inverse(M):
     """
