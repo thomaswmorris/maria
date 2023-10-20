d4cc477b9ff65d94ecc1a47fd1d7eab0873593d2
Thomas Morris <thomasmorris@princeton.edu>
2023-10-19 23:31:28 -0400

    added more sites
    Former-commit-id: 26ea3fcadc653cf5747bbfcb890968c357ee88f8 [formerly ad889a48a0b5143e0b05c3775a71657972bd3dad] [formerly 109f3d11cd8dfc7eb441129bb6239aaae6e78372 [formerly 58af3b6e8f53bcb975f360f15979cd7396d14965] [formerly 109f3d11cd8dfc7eb441129bb6239aaae6e78372 [formerly 58af3b6e8f53bcb975f360f15979cd7396d14965] [formerly e9da0bcd81dc4d8d64e0e8db40f0ff4ca29b26d2]]]
    Former-commit-id: 24a649019e0570ab941f220f998e473f703585bf [formerly 024d38c1caba9fac06a457e5d42c8234d1a621c8]
    Former-commit-id: afe44691a14e54c1a0221f7fd52b260da1cb97ca
    Former-commit-id: ec7329b8835279bb5ceaceb6af9ed41f81e5ba21
    Former-commit-id: 3aa860f1f73d41c3254af62a60c472bbca347320

--- maria/tests/test_noise_sims.py
+++ maria/tests/test_noise_sims.py
@@ -6,7 +6,7 @@ from maria.noise import NoiseSimulation
 @pytest.mark.noise
 def test_linear_angular_model():
 
-    sim = NoiseSimulation(array="default", pointing="default", site="default")
+    sim = NoiseSimulation(array="MUSTANG-2", pointing="daisy", site="GBT")
     tod = sim.run()
 
 
