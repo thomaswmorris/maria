24139ec44e80b5c3356f447ef548c283052fa8be
Thomas Morris <thomasmorris@princeton.edu>
2023-10-31 22:54:15 -0400

    add multi-layer simulation
    Former-commit-id: 18972b33d89b7a8e112ee7e98c8c084a7499fe16 [formerly 9214973681eb83217464225b9bdb27077d5927ab] [formerly 099f2b69e70dc0e86aa36854439d2d6ff2059bd3 [formerly c29b94cd8072906a7bde8cdc4bd637a7a23eeabd] [formerly 099f2b69e70dc0e86aa36854439d2d6ff2059bd3 [formerly c29b94cd8072906a7bde8cdc4bd637a7a23eeabd] [formerly 775ccad497dee83e59bf1841ea74a8405c334f69]]]
    Former-commit-id: 087a9ff86f19a777a4106b4cfdcc6deba91a79f9 [formerly d750a1d3b6e8d2a5f1b251d042b910de502dac9b]
    Former-commit-id: 42af29e07fe9246f181ead9568651c1cfa9df5e1
    Former-commit-id: 5c47f8ef306c4cd5c8de340ab022fefd6561f14d
    Former-commit-id: 3b6fd07409c3758dac0853721053b1e889151d9f

--- maria/__init__.py
+++ maria/__init__.py
@@ -8,7 +8,8 @@ __version__ = get_versions()["version"]
 del get_versions
 
 from . import array, pointing, site, utils, weather  # noqa F401
-from .array import get_array  # noqa F401
-from .pointing import get_pointing  # noqa F401
+from .array import all_arrays, get_array  # noqa F401
+from .pointing import all_pointings, get_pointing  # noqa F401
 from .sim import Simulation  # noqa F401
-from .site import get_site  # noqa F401
+from .site import all_sites, get_site  # noqa F401
+from .weather import all_regions  # noqa F401
---
+++ maria/utils/constants.py
@@ -0,0 +1,10 @@
+# little g (m s^-2)
+g = 9.806651
+
+# Planck's constant (kg m^2 s)
+h = 6.62607015e-34
+
+# Boltzmann's constant (kg m^2 s^-2 K^-1)
+k_B = 1.380649e-23
+
+T_CMB = 2.7255
