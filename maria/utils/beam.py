16cc590b0fbc32943228f2c2124b60fd06028d35
Thomas Morris <thomasmorris@princeton.edu>
2023-10-31 23:07:46 -0400

    run pre-commit
    Former-commit-id: 516f91be011d94b67b679b01d0d670ec9644cd1c [formerly a78c922fad56546908e5c1a684ed96d9279f25bc] [formerly f2643a8def22061cd8ed30bf2720677995b42a94 [formerly 872755502acbb98526bbdd9231baa6552e1c7927]]
    Former-commit-id: bf3f7de4be6d43fe38af7dc3751f16b410a73dcd [formerly eb54cd89c7f26bdd4429cbfa05d3c1b6827d6d79]
    Former-commit-id: 2fde2006eb21be72dea5c2477201972a79d41fbb

--- maria/utils/beam.py
+++ maria/utils/beam.py
@@ -10,7 +10,7 @@ def gaussian_beam_angular_fwhm(z, w_0, n=1, f=None, l=None):  # noqa F401
     NOTE: The waist of a beam is half the diameter at its smallest point.
     For telescope purposes, this will be half the width of the objective/primary.
     """
-    l = l if l is not None else 2.998e8 / (1e9 * f)
+    l = l if l is not None else 2.998e8 / (1e9 * f)  # noqa F401
 
     # Rayleigh range
     z_r = np.pi * w_0**2 * n / l
@@ -18,7 +18,7 @@ def gaussian_beam_angular_fwhm(z, w_0, n=1, f=None, l=None):  # noqa F401
     return np.sqrt(2 * np.log(2)) * w_0 * np.sqrt(1 / z**2 + 1 / z_r**2)
 
 
-def gaussian_beam_physical_fwhm(z, w_0, n=1, f=None, l=None):
+def gaussian_beam_physical_fwhm(z, w_0, n=1, f=None, l=None):  # noqa F401
     return z * gaussian_beam_angular_fwhm(z=z, w_0=w_0, n=n, f=f, l=l)  # noqa F401
 
 
