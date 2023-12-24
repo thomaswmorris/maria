32555d35418d448afa0466bef6239739c8000ca8
Thomas Morris <thomasmorris@princeton.edu>
2023-12-24 17:49:31 -0500

    refactored TOD object
    Former-commit-id: eb6b3215933f62eb82fd0e4756ce571b7b58f245 [formerly 87c1d83f0f99630d4d4858e2b4c625fa634ae62e] [formerly 32b7f820ec8b89496b24919e9403ea38cf2cae4e [formerly 63636facdd45ffefcb74f43c6bd4d3671c523834] [formerly 32b7f820ec8b89496b24919e9403ea38cf2cae4e [formerly 63636facdd45ffefcb74f43c6bd4d3671c523834] [formerly 3d86828e1193e6f14a7fa176689ac74be268cfdc]]]
    Former-commit-id: 8490bb8bea21e171f8ad0d6c16883abcdbeaa1f2 [formerly 6d302dc7e11504e269e4a5679d0604851413b9c0]
    Former-commit-id: 2f3b21aa089f4cdb32cf58ae9c0a5d9a8dfdccd4
    Former-commit-id: 2701e77d0a9eba36b602923a18a8e5ce83e6eccd
    Former-commit-id: e8cdf191d9770dccacd09112691ebc8afaeeb194

--- maria/__init__.py
+++ maria/__init__.py
@@ -9,6 +9,7 @@ del get_versions
 
 from . import array, pointing, site, utils, weather  # noqa F401
 from .array import all_arrays, get_array  # noqa F401
+from .map import Map, mappers  # noqa F401
 from .pointing import all_pointings, get_pointing  # noqa F401
 from .sim import Simulation  # noqa F401
 from .site import all_sites, get_site  # noqa F401
