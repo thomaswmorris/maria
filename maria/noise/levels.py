39c21d1283f99459cf7aa3d5b020860f8d5b9c7c
Joshiwavm <joshiwa01@gmail.com>
2024-02-20 12:48:34 +0100

    added csv file to noise * calibration to it
    Also added units back to mapper. The automatic update to Jy/pixel didn't seem to work

--- maria/noise/levels.py
+++ maria/noise/levels.py
@@ -14,15 +14,19 @@ class ReadOutBLIPs:
 
     def _get_levels(self):
         df = pd.read_csv(
-            "../sens_calc/results_of_instrument_simulation.csv", index_col=0
+            "./maria/noise/results_of_instrument_simulation.csv", index_col=0
         )
         frq_index = np.argmin(np.abs(self.f_c - df["fmin"]))
 
         used_b_w = df["fmax"][frq_index] - df["fmin"][frq_index]  # GHz
         NET = df["NET_RJ of a Single Detector"][frq_index]
 
-        self.white_noise_RJ = NET / np.sqrt(self.b_w / used_b_w)  # uK s^0.5
-        self.pink_noise_RJ = 0.3 * (self.white_noise_RJ / 112.0)
+        self.white_noise_RJ = NET / np.sqrt(self.b_w / used_b_w) * 1e-6  # K s^0.5
+        self.pink_noise_RJ = 0.3 * (self.white_noise_RJ / 112.0e-6)
+
+        if self.cal:
+            self.white_noise_RJ = self.white_noise_RJ * 0.3 / self.abscal
+            self.pink_noise_RJ = self.pink_noise_RJ * 0.3 / self.abscal
 
 
 class InitNoise(ReadOutBLIPs):
@@ -34,6 +38,8 @@ class InitNoise(ReadOutBLIPs):
         self,
         f_c: np.float32 = 93,  # GHz -- central frequency
         b_w: np.float32 = 52,  # GHz -- band width
+        abscal: np.float32 = 1.0,  # Calibration term due to atmospheric absorption
+        cal: bool = True,
         *args,
         **kwargs,
     ):
@@ -41,4 +47,6 @@ class InitNoise(ReadOutBLIPs):
 
         self.f_c = f_c
         self.b_w = b_w
+        self.cal = cal
+        self.abscal = abscal
         self._run()
