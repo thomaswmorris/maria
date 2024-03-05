a6693a27462837743ff53179c7c81d5ab7889684
Thomas Morris <thomasmorris@princeton.edu>
2024-03-04 21:23:38 -0500

    use power units under the hood
    Former-commit-id: 48ffff858e98565a5625eaf820b52bb3ae425793 [formerly 4d48a2ac4660aafd404c04c3e0fe814374be3098] [formerly 48ffff858e98565a5625eaf820b52bb3ae425793 [formerly 4d48a2ac4660aafd404c04c3e0fe814374be3098] [formerly 3f92ef077d944f8b18a5337977e2e3d1d9d8164a]]
    Former-commit-id: a05e0ede1c37d47bd5f5f9bf5577c30d96a0d14b
    Former-commit-id: a5d084efcf2d5042435f3341640f6b1d2ca55336 [formerly ac8d158ede2d203cc0a75dadc604329905c9c799] [formerly ebd49c732783a81c2067c87122677c6eb31b87af]
    Former-commit-id: 6bfc9873cddbf69ed1b0535ccceb1e7503029dd0
    Former-commit-id: 1e84f6188829d8d9a0619e474796a2f632f1334d

--- maria/utils/functions.py
+++ maria/utils/functions.py
@@ -29,16 +29,19 @@ def normalized_matern(r, nu):
     )
 
 
-def approximate_normalized_matern(r, nu, n_test_points=1024):
+def approximate_normalized_matern(r, nu, n_test_points=256):
     """
     Computing BesselK[nu,z] for arbitrary nu is expensive. This is good for casting over huge matrices.
     """
 
     r_safe = np.atleast_1d(np.abs(r))
-    r_min = np.maximum(r_safe[r_safe > 0].min(), 1e-6)
-    r_max = np.minimum(r_safe.max(), 1e3)
+    r_min = np.maximum(1e-6, 0.5 * r_safe[r_safe > 0].min())
+    r_max = np.minimum(1e3, 2.0 * r_safe.max())
 
-    test_values = np.r_[0, np.geomspace(r_min, r_max, n_test_points - 1)]
-    data_values = normalized_matern(test_values, nu) * np.exp(test_values)
+    r_samples = np.geomspace(r_min, r_max, n_test_points)
 
-    return np.interp(r_safe, test_values, data_values) * np.exp(-r_safe)
+    sf_samples = 1 - normalized_matern(r_samples, nu=nu)
+
+    sf = np.exp(np.interp(np.log(r), np.log(r_samples), np.log(sf_samples)))
+
+    return 1 - sf
