fix units bug
Former-commit-id: ada25366518e6d1739df7be51f61c7583bf7f7b7 [formerly 2a78f31eba471f9cd4182af29196a777f74ef997]
Former-commit-id: 79f1895b8670c865e94ed8288056201730fb67c4

maria/instrument/__init__.py
@@ -84,7 +84,13 @@ class Instrument:
         return cls(bands=dets.bands, dets=dets, **config)
 
     def __post_init__(self):
-        self.field_of_view = np.round(self.dets.sky_x.ptp(), 3)
+        self.field_of_view = np.round(np.degrees(self.dets.sky_x.ptp()), 3)
+        if self.field_of_view < 0.5 / 60:
+            self.units = "arcsec"
+        elif self.field_of_view < 0.5:
+            self.units = "arcmin"
+        else:
+            self.units = "degrees"
 
     def __repr__(self):
         nodef_f_vals = (
@@ -168,7 +174,10 @@ class Instrument:
     #     """
     #     return construct_beam_filter(self.physical_fwhm(z), res, beam_profile=beam_profile, buffer=buffer)
 
-    def plot_dets(self, units="deg"):
+    def plot_dets(self, units=None):
+        if units is None:
+            units = self.units
+
         fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=160)
 
         legend_handles = []

maria/map/__init__.py
@@ -1,17 +1,30 @@
+import os
 from dataclasses import dataclass, fields
 from operator import attrgetter
 from typing import List, Tuple
 
 import astropy as ap
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from astropy.io import fits
 from astropy.wcs import WCS
+from matplotlib.colors import ListedColormap
 from tqdm import tqdm
 
 from .. import utils
 from ..instrument import beams
+
+here, this_filename = os.path.split(__file__)
+
+# from https://gist.github.com/zonca/6515744
+cmb_cmap = ListedColormap(
+    np.loadtxt(f"{here}/Planck_Parchment_RGB.txt") / 255.0, name="cmb"
+)
+cmb_cmap.set_bad("white")
+
+mpl.colormaps.register(cmb_cmap)
 
 
 @dataclass
@@ -32,6 +45,7 @@ class Map:
     frame: str = "ra_dec"
     units: str = "K_RJ"
     data: np.array = None  # 3D instrument
+    weight: np.array = None
 
     def __post_init__(self):
         assert len(self.freqs) == self.n_freqs
@@ -40,6 +54,9 @@ class Map:
 
         self.width = self.res * self.n_x
         self.height = self.res * self.n_y
+
+        if self.weight is None:
+            self.weight = np.ones(self.data.shape)
 
     def __repr__(self):
         nodef_f_vals = (
@@ -88,7 +105,9 @@ class Map:
         y = self.res * np.arange(self.n_y)
         return y - y.mean()
 
-    def plot(self, cmap="plasma", units="degrees", **kwargs):
+    def plot(
+        self, cmap="cmb", rel_vmin=0.001, rel_vmax=0.999, units="degrees", **kwargs
+    ):
         for i_freq, freq in enumerate(self.freqs):
             header = fits.header.Header()
 
@@ -135,7 +154,13 @@ class Map:
             if units == "arcsec":
                 map_extent = 3600 * np.degrees(map_extent_radians)
 
-            vmin, vmax = np.nanpercentile(self.data, q=[1, 99])
+            d = self.data.ravel()
+            w = self.weight.ravel()
+
+            sort = np.argsort(d)
+            d, w = d[sort], w[sort]
+
+            vmin, vmax = np.interp([rel_vmin, rel_vmax], np.cumsum(w) / np.sum(w), d)
 
             map_im = ax.imshow(
                 self.data.T,

maria/map/mappers.py
@@ -158,7 +158,8 @@ class BinMapper(BaseMapper):
         self.raw_map_sums = {band: np.zeros((self.n_x, self.n_y)) for band in self.band}
         self.raw_map_cnts = {band: np.zeros((self.n_x, self.n_y)) for band in self.band}
 
-        self.map_data = np.zeros((len(self.band), self.n_x, self.n_y))
+        self.map_data = np.zeros((len(self.band), self.n_y, self.n_x))
+        self.map_weight = np.zeros((len(self.band), self.n_y, self.n_x))
 
         for iband, band in enumerate(np.unique(self.band)):
             self.band_data[band] = {}
@@ -258,8 +259,11 @@ class BinMapper(BaseMapper):
                 mask, band_map_denom, 1
             )
 
+            self.map_weight[iband] = band_map_denom
+
         self.map = Map(
             data=self.map_data,
+            weight=self.map_weight,
             freqs=np.array([self.band_data[band]["band_center"] for band in self.band]),
             width=np.degrees(self.width) if self.degrees else self.width,
             height=np.degrees(self.height) if self.degrees else self.height,