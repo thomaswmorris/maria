6714ed3bd6a9889e1e81f62fc924d88258147ef9
Thomas Morris <thomasmorris@princeton.edu>
2024-05-07 22:28:23 -0400

    bugs
    Former-commit-id: 0828bce4688232bf01bc74e7ef4e137ddb1f2a02 [formerly 90911de4f44551a4509e104c365f03ad1c2337e0] [formerly 0828bce4688232bf01bc74e7ef4e137ddb1f2a02 [formerly 90911de4f44551a4509e104c365f03ad1c2337e0] [formerly 6f44fbe2b391483aa68ebed540d9bf779c465773]]
    Former-commit-id: 0854b562d68f5914c84748177ca926364fc1fb47
    Former-commit-id: c3fdf3c15387e920bd938c884ddb6e17621b1ab8
    Former-commit-id: d30ff6e7f134ef04f303d01a67347628818dcf0b

--- maria/tests/test_tod.py
+++ maria/tests/test_tod.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from maria.tod import TOD
+from maria.tod.mustang2 import load_mustang2_tod
 
 from ..utils.io import fetch_cache
 
@@ -13,5 +13,5 @@ TEST_MUSTANG_TOD_URL = "https://github.com/thomaswmorris/maria-data/raw/master/t
 
 def test_tod_io_fits_mustang2():
     fetch_cache(TEST_MUSTANG_TOD_URL, "/tmp/test_m2_tod.fits", refresh=True)
-    tod = TOD.from_fits(fname="/tmp/test_m2_tod.fits", format="mustang-2")
-    tod.to_fits(fname="/tmp/test_save_m2_tod.fits", format="mustang-2")
+    tod = load_mustang2_tod(fname="/tmp/test_m2_tod.fits")
+    tod.to_fits(fname="/tmp/test_save_m2_tod.fits")
