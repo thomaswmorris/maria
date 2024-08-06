8e42c1b14edb32279d13622507f6cd071d74fcda
Thomas Morris <thomas.w.morris@yale.edu>
2024-08-05 22:43:27 -0400

    bug fixes
    Former-commit-id: fe2f99ef4a291e1b800aae403bdef8d6af28ef0d [formerly 0ddafc7eeed88b414aff829b4809e0f2cdc08c3e] [formerly 6f01ca420414eec511a7312c407a4581439d5030 [formerly 7f3d5cb6dd55e9d970d7a043e6e4a8bc7862150a] [formerly 6f01ca420414eec511a7312c407a4581439d5030 [formerly 7f3d5cb6dd55e9d970d7a043e6e4a8bc7862150a] [formerly 7eb6381124ba427178be94bd54e1e01f3abac444]]]
    Former-commit-id: c1e184c49781f9bca4897eebdf9e4a3eb47c993d [formerly 246c775f24070c135f0e14bf875b8cf17a369f24]
    Former-commit-id: 21bd0c1a6b62d2570ee0a9e4e7e36d60a542c1de
    Former-commit-id: a7a6aa158eda2711627e5a210511edad202d004b [formerly ae5c99218f2988a3aef4017f40a59672ff723166] [formerly 5d717e6807d6fb1e04554317163e52cf6cb5419d]
    Former-commit-id: 776f81c94f95f868a7e422788f1925b4a9767497
    Former-commit-id: 5644483923536444b049605b2817b155060b8a6a

---
+++ maria/coords/__init__.py
@@ -0,0 +1,45 @@
+import time as ttime
+
+from .coordinates import Coordinates  # noqa
+from .transforms import (  # noqa
+    dx_dy_to_phi_theta,
+    get_center_phi_theta,
+    phi_theta_to_dx_dy,
+    phi_theta_to_xyz,
+    xyz_to_phi_theta,
+)
+
+
+def now():
+    return ttime.time()
+
+
+frames = {
+    "az_el": {
+        "astropy_name": "altaz",
+        "astropy_phi": "az",
+        "astropy_theta": "alt",
+        "phi": "az",
+        "theta": "el",
+        "phi_name": "RA",
+        "theta_name": "Dec.",
+    },
+    "ra_dec": {
+        "astropy_name": "icrs",
+        "astropy_phi": "ra",
+        "astropy_theta": "dec",
+        "phi": "ra",
+        "theta": "dec",
+        "phi_name": "RA",
+        "theta_name": "Dec.",
+    },
+    "galactic": {
+        "astropy_name": "galactic",
+        "astropy_phi": "l",
+        "astropy_theta": "b",
+        "phi": "l",
+        "theta": "b",
+        "phi_name": "RA",
+        "theta_name": "Dec.",
+    },
+}
