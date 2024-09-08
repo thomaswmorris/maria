7032d66ce79c36e9312d3f10d4e51f60a6faab99
Thomas Morris <thomas.w.morris@yale.edu>
2024-09-07 23:33:49 -0400

    fix string formatting
    Former-commit-id: 4101a2c4e00f686d446632102d6fa1e73805c1da [formerly ad44f9dca27d919c9af431051a00318aedff3a51] [formerly 2ea77194620116875714d17f8189faed53d0f6ed [formerly 654bf00d50681a998a9005d94a59e87736bd0fe7] [formerly 2ea77194620116875714d17f8189faed53d0f6ed [formerly 654bf00d50681a998a9005d94a59e87736bd0fe7] [formerly 81a6901bc27fe5b48bb70646efc92580ee3fa27f]]]
    Former-commit-id: e20ec97954e2e5c8b4ea2326e448bb4a929aa786 [formerly 8449207ab9be1061cec2a1d98dfe094369766d2f]
    Former-commit-id: 62b21f5fd5d8a33ce40167c6c290bb2057e7159b
    Former-commit-id: e5a2425ecceffe925fe616197558d8683aac4824
    Former-commit-id: 9d4b5f90679415972aabc0bdc21464671682cdd7

--- maria/tests/test_weather.py
+++ maria/tests/test_weather.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pytest
 
+import maria
 from maria.weather import Weather
 
 
-@pytest.mark.parametrize("region_name", ["chajnantor"])
+@pytest.mark.parametrize("region_name", maria.all_regions)
 def test_weather_from_cache(region_name):
     weather = Weather(region=region_name, refresh_cache=True)
 
