ba31bb200d0f41b6dc275c3ecde41f09802b2707
Thomas Morris <thomas.w.morris@yale.edu>
2024-09-07 23:27:42 -0400

    add gain errors
    Former-commit-id: 15c30ac3171423bd0b7597e96facce892a785358 [formerly 0148128dd91f4a63e8c3146c784ef6382baa0868] [formerly 754b69ec02355ea232f6a7885a1f1269a2a960bd [formerly 39277750ec651761c6e7ac98f50d8fc0c3a6cc7a] [formerly 754b69ec02355ea232f6a7885a1f1269a2a960bd [formerly 39277750ec651761c6e7ac98f50d8fc0c3a6cc7a] [formerly b44dac8c32bf2bd504ef3bcacb217541ae77539f]]]
    Former-commit-id: bbdf54af39c41e226abdfc4de0ec0ef133c84bdb [formerly 6c630b1c1f4bdf60299cb3f827f698ad3a2aabd2]
    Former-commit-id: 5987658d50e6f25877537493804d885e88a0ede8
    Former-commit-id: c8e42fefa9bdf440d78bb8d11577e320a7debbbc [formerly 730a1f81ef8fc0be7e3a4c13f599512a4127e4cf] [formerly 036b3efe0f71d65ea46bb2193b69b99c2e7c5406]
    Former-commit-id: d79e453dc943472cdd2c49442e4ebfe8939d4603
    Former-commit-id: 8e70f9dc060ae2ad114f22650cb16002e8a13b16

--- maria/tests/test_plans.py
+++ maria/tests/test_plans.py
@@ -1,11 +1,10 @@
 import pytest
 
 import maria
+from maria.plan import PATTERNS
 
 
-@pytest.mark.parametrize(
-    "scan_pattern", ["stare", "daisy", "raster", "grid", "back_and_forth"]
-)
+@pytest.mark.parametrize("scan_pattern", PATTERNS.index)
 def test_pattern(scan_pattern):
     plan = maria.Plan(scan_pattern=scan_pattern)
     print(plan)
