d759bad3fff9d2e438b8bc1ebb0f6a44068480ab
Thomas Morris <thomas.w.morris@yale.edu>
2024-10-16 18:36:26 -0400

    add sites
    Former-commit-id: 31b66c093679c14dbd07e286890e5d3cba19d6ce [formerly 3dbd47c0536a3728c42e4f096f0ace5590ee0209] [formerly 87cdb76872f3a1557fe00c2144a3eccf4a0eec62 [formerly e188d0f5d2af94c780146bf8789445177e925b86] [formerly 87cdb76872f3a1557fe00c2144a3eccf4a0eec62 [formerly e188d0f5d2af94c780146bf8789445177e925b86] [formerly db627f8239fa57750b881da46158e32ed20e34fb]]]
    Former-commit-id: ad65901d53d8461068fc9121dd73f5d198d0c0b7 [formerly a8069dff24f358be1e476851a5d68cd480e40327]
    Former-commit-id: 58bce155fb25a0d5cef5280a1a11423864e8ef6c
    Former-commit-id: cebfac7ff5c8c107b47e86e0d22d995d53bcb790
    Former-commit-id: 39b3fdb5f88c00efa8ddf66d4b3040037a329606

--- maria/tests/test_io.py
+++ maria/tests/test_io.py
@@ -1,25 +1,6 @@
-import time
-
 import pytest
 
-import maria
-from maria.atmosphere import Atmosphere, AtmosphericSpectrum, Weather
 from maria.io import fetch
-
-
-@pytest.mark.parametrize("region_name", maria.all_regions)
-def test_atmosphere(region_name):
-    Atmosphere(region=region_name)
-
-
-@pytest.mark.parametrize("region_name", ["chajnantor"])
-def test_spectrum_from_cache(region_name):
-    AtmosphericSpectrum(region=region_name, refresh_cache=True)
-
-
-@pytest.mark.parametrize("region_name", ["chajnantor"])
-def test_weather_from_cache(region_name):
-    Weather(region=region_name, refresh_cache=True)
 
 
 @pytest.mark.parametrize("filename", ["cluster.fits"])
