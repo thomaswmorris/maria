b83e22570cc6603914f15e3dca38bc169efaeccc
Thomas Morris <thomas.w.morris@yale.edu>
2024-10-29 17:04:24 -0400

    update docs
    Former-commit-id: 11d4e4ef07226022b92d4cca5f1820890186bd65 [formerly cb1cc45620552048be9666daa60b5ad6016bfd5a] [formerly a5064f36693f909e77e0fed1f3a3e835b5e6de54 [formerly 251d649c52361bde36f43a61ced820d201e8620a] [formerly a5064f36693f909e77e0fed1f3a3e835b5e6de54 [formerly 251d649c52361bde36f43a61ced820d201e8620a] [formerly 599a9caf19378c52ac389e7ab251937a8a45ce4d]]]
    Former-commit-id: 1f89fc9aeaeb6f41df219a9c4078dca797fb1f29 [formerly 06a4f3ab537a1fdc916b54526eff40ba24fa3eef]
    Former-commit-id: ace02b935e060001d3551fd8a4fafc72615befa7
    Former-commit-id: 601907f8cc6d69e1b2800be94fdc225d22ee4b82 [formerly 4f502a12c9e4b64eb30558c801c78defad155ac1] [formerly 0c68a97c1c5eff5f633d0d5e0862c96d6eff81fb]
    Former-commit-id: 925970862c3bfc01fed31e3d6c61fc3c705b67d7
    Former-commit-id: eb91bc73e237edf2baafda0df4b5aecd3f0eb2d4

--- maria/tests/test_spectrum.py
+++ maria/tests/test_spectrum.py
@@ -1,12 +1,16 @@
-import time
-
 import pytest
 
 import maria
-from maria.atmosphere import Atmosphere, AtmosphericSpectrum, Weather
-from maria.io import fetch
+from maria.atmosphere import AtmosphericSpectrum
 
 
 @pytest.mark.parametrize("region_name", ["chajnantor"])
+def test_spectrum_from_cache_refresh(region_name):
+    spectrum = AtmosphericSpectrum(region=region_name, refresh_cache=True)
+
+
+@pytest.mark.parametrize("region_name", maria.all_regions)
 def test_spectrum_from_cache(region_name):
-    AtmosphericSpectrum(region=region_name, refresh_cache=True)
+    spectrum = AtmosphericSpectrum(region=region_name)
+
+    spectrum.emission(nu=90, elevation=45, pwv=1.5)
--- maria/tests/test_weather.py
+++ maria/tests/test_weather.py
@@ -5,8 +5,13 @@ import maria
 from maria.weather import Weather
 
 
+@pytest.mark.parametrize("region_name", ["chajnantor"])
+def test_weather_from_cache_refresh(region_name):
+    weather = Weather(region=region_name, refresh_cache=True)
+
+
 @pytest.mark.parametrize("region_name", maria.all_regions)
 def test_weather_from_cache(region_name):
-    weather = Weather(region=region_name, refresh_cache=True)
+    weather = Weather(region=region_name)
 
     print(f"{weather.pwv = }")
