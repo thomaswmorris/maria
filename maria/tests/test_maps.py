d96395f69666c70c181e498d7312dee9d3599622
Thomas Morris <thomas.w.morris@yale.edu>
2024-11-16 16:15:56 -0500

    fix opacity computation
    Former-commit-id: 3df5d67aa247ca828159ebfbe73ad06723bda986 [formerly d7ff97318901c80faf3dd0e8b504596bda1705d2] [formerly 3c9b0e42b210d6c4312d718547d28d5248264c79 [formerly 6765c4dfc4d8212bdac4967dd21062e3bb76f4ba] [formerly 3c9b0e42b210d6c4312d718547d28d5248264c79 [formerly 6765c4dfc4d8212bdac4967dd21062e3bb76f4ba] [formerly 33e465471c0188b18a0517df2816fb40dbd52373]]]
    Former-commit-id: 88232d5bd5c9fff877fddf6f0b1e459eca49b728 [formerly 0ff76a924ca50f73aabb4b1bd21134f743a2df1b]
    Former-commit-id: 4d3c751638a7b5876e5b11c9b2a81aca565f806c
    Former-commit-id: 2d9d06d411ceef07873edeb98dae6c4e76a2ffe1
    Former-commit-id: 17631ce60346c085f5d1eb87ee3ea3f8c4a69e05

--- maria/tests/test_maps.py
+++ maria/tests/test_maps.py
@@ -9,13 +9,11 @@ plt.close("all")
 
 @pytest.mark.parametrize(
     "map_name", ["maps/cluster.fits", "maps/big_cluster.fits", "maps/galaxy.fits"]
-)
-def test_atmosphere(map_name):
+)  # noqa
+def test_fetch_fits_map(map_name):
     map_filename = fetch(map_name)
-
-    m = maria.map.read_fits(
+    m = maria.map.load(
         filename=map_filename,
-        index=0,
         nu=90,
         resolution=1 / 1024,
         center=(150, 10),
@@ -23,3 +21,19 @@ def test_atmosphere(map_name):
     )
 
     m.plot()
+
+
+@pytest.mark.parametrize("map_name", ["maps/sun.h5"])  # noqa
+def test_fetch_hdf_map(map_name):
+    map_filename = fetch(map_name)
+    m = maria.map.load(filename=map_filename)
+    m.plot()
+
+
+def test_time_ordered_map_sim():
+    map_filename = fetch("maps/sun.h5")
+    m = maria.map.load(filename=map_filename)
+    plan = maria.Plan(start_time=0)
+    sim = maria.Simulation(plan=plan, map=m)
+    tod = sim.run()
+    tod.plot()
