fix opacity computation
Former-commit-id: 3df5d67aa247ca828159ebfbe73ad06723bda986 [formerly d7ff97318901c80faf3dd0e8b504596bda1705d2] [formerly 6765c4dfc4d8212bdac4967dd21062e3bb76f4ba [formerly 33e465471c0188b18a0517df2816fb40dbd52373]]
Former-commit-id: 88232d5bd5c9fff877fddf6f0b1e459eca49b728 [formerly 0ff76a924ca50f73aabb4b1bd21134f743a2df1b]
Former-commit-id: 4d3c751638a7b5876e5b11c9b2a81aca565f806c

maria/atmosphere/extrusion.py
@@ -17,7 +17,7 @@ logger = logging.getLogger("maria")
 
 
 MIN_RES = {"2d": 5, "3d": 15}
-MIN_RES_PER_BEAM = {"2d": 0.25, "3d": 0.5}
+MIN_RES_PER_BEAM = {"2d": 0.1, "3d": 0.5}
 MIN_RES_PER_FOV = {"2d": 0.05, "3d": 0.1}
 
 
@@ -25,7 +25,7 @@ def generate_layers(
     sim,
     mode: str = "2d",
     angular: bool = True,
-    h_max: float = 2e3,  # in meters
+    max_height: float = 2e3,  # in meters
     min_res: float = None,  # in meters
     min_res_per_beam: float = None,
     min_res_per_fov: float = None,
@@ -42,7 +42,7 @@ def generate_layers(
 
     min_el = sim.boresight.el.min().compute()
 
-    h_samples = np.arange(0, h_max + 1e0, 1e-1)
+    h_samples = np.arange(0, max_height + 1e0, 1e-1)
 
     z_samples = h_samples / np.sin(min_el)
 
@@ -61,13 +61,13 @@ def generate_layers(
         return sp.interpolate.interp1d(h_samples, res_samples)(h)
 
     if mode == "2d":
-        h_boundaries = np.arange(0, h_max + layer_spacing, layer_spacing)
+        h_boundaries = np.arange(0, max_height + layer_spacing, layer_spacing)
         process_index = np.arange(len(h_boundaries) - 1)
     elif mode == "3d":
         h_boundaries = [0]
         while True:
             new_h = h_boundaries[-1] + res_func(h_boundaries[-1])
-            if new_h > h_max:
+            if new_h > max_height:
                 break
             h_boundaries.append(h_boundaries[-1] + res_func(h_boundaries[-1]))
         h_boundaries = np.array(h_boundaries)

maria/instrument/band/__init__.py
@@ -6,6 +6,7 @@ from typing import Sequence, Union
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy as sp
 
 from ...functions import planck_spectrum
 from ...io import flatten_config, read_yaml
@@ -85,14 +86,35 @@ def parse_tod_calibration_signature(s):
     raise ValueError("Calibration must have signature 'units1 -> units2'.")
 
 
+def generate_passband(center, width, shape, samples=256):
+    if shape == "flat":
+        nu_min, nu_max = (center - 0.6 * width, center + 0.6 * width)
+    else:
+        nu_min, nu_max = (center - 1.5 * width, center + 1.5 * width)
+
+    nu = np.linspace(nu_min, nu_max, samples)
+
+    if shape == "flat":
+        tau = np.where((nu > center - 0.5 * width) & (nu < center + 0.5 * width), 1, 0)
+    elif shape == "gaussian":
+        tau = np.exp(np.log(0.5) * (2 * (nu - center) / width) ** 2)
+    elif shape == "top_hat":
+        tau = np.exp(np.log(0.5) * (2 * (nu - center) / width) ** 8)
+    else:
+        raise ValueError(f"Invalid shape '{shape}'.")
+    return nu, tau
+
+
 class Band:
     def __init__(
         self,
-        center: float,
-        name: str = None,
+        center: float = None,
         width: float = None,
-        shape: str = "gaussian",
-        efficiency: float = 0.5,
+        nu: float = None,
+        tau: float = None,
+        name: str = None,
+        shape: str = "top_hat",
+        efficiency: float = 1.0,
         sensitivity: float = None,
         sensitivity_kind: str = "rayleigh-jeans",
         gain_error: float = 0,
@@ -101,11 +123,34 @@ class Band:
         knee: float = 1.0,
         time_constant: float = 0.0,
     ):
-        self.center = float(center)
+        auto = center is not None and width is not None
+        manual = nu is not None and tau is not None
 
-        self.name = name or f"f{int(self.center):>03}"
-        self.width = width or 0.1 * self.center
+        if not auto ^ manual:
+            raise ValueError(
+                "You must pass either both 'center' and 'width' or both 'nu' and 'tau'."
+            )
 
+        if auto:
+            self.nu, self.tau = generate_passband(center, width, shape, samples=64)
+
+        if manual:
+            tau_max = np.max(tau)
+            efficiency *= tau_max
+
+            self.nu = np.array(nu)
+            self.tau = np.array(tau) / tau_max
+
+            if (
+                (self.nu.ndim != 1)
+                or (self.tau.ndim != 1)
+                or (self.nu.shape != self.tau.shape)
+            ):
+                raise ValueError(
+                    f"'nu' and 'tau' have mismatched shapes ({self.nu.shape} and {self.tau.shape})."
+                )
+
+        self._name = name
         self.shape = shape
         self.efficiency = efficiency
         self.NEP_per_loading = NEP_per_loading
@@ -130,6 +175,28 @@ class Band:
             self.set_sensitivity(
                 sensitivity, kind=sensitivity_kind
             )  # this sets the NEP automatically
+
+    @property
+    def name(self):
+        return self._name or f"f{int(self.center):>03}"
+
+    @property
+    def center(self):
+        return np.round(np.sum(self.nu * self.tau) / np.sum(self.tau), 2)
+
+    @property
+    def width(self):
+        return np.round(self.fwhm, 2)
+
+    @property
+    def fwhm(self):
+        crossovers = np.where((self.tau[1:] > 0.5) != (self.tau[:-1] > 0.5))[0]
+        return np.ptp(
+            [
+                sp.interpolate.interp1d(self.tau[[i, i + 1]], self.nu[[i, i + 1]])(0.5)
+                for i in crossovers
+            ]
+        )
 
     @property
     def summary(self):
@@ -165,11 +232,9 @@ class Band:
         return f"Band({', '.join(parts)})"
 
     def plot(self):
-        nus = np.linspace(self.nu_min, self.nu_max, 256)
-
         fig, ax = plt.subplots(1, 1)
 
-        ax.plot(nus, self.passband(nus), label=self.name)
+        ax.plot(self.nu, self.tau, label=self.name)
 
         ax.set_xlabel(r"$\nu$ [GHz]")
         ax.set_ylabel(r"$\tau(\nu)$ [Rayleigh-Jeans]")
@@ -207,58 +272,24 @@ class Band:
         return self.spectrum.transmission(nu=self.center, pwv=pwv, elevation=elevation)
 
     @classmethod
-    def from_config(cls, name, config):
-        if "passband" in config:
-            df = pd.read_csv(f"{here}/{config.pop('passband')}", index_col=0)
+    def from_file(cls, filename, **kwargs):
+        path = (
+            f"{here}/{filename}" if os.path.exists(f"{here}/{filename}") else filename
+        )
 
-            nu, pb = df.nu.values, df.passband.values
+        df = pd.read_csv(path, index_col=0)
 
-            center = np.round(np.sum(pb * nu), 3)
-            width = np.round(np.ptp(nu[pb > 1e-2 * pb.max()]), 3)
+        nu, tau = df.nu.values, df.passband.values
 
-            band = cls(name=name, center=center, width=width, shape="custom", **config)
-            band._nu = nu
-            band._pb = pb
-        else:
-            band = cls(name=name, **config)
-
-        return band
-
-    @property
-    def nu_min(self) -> float:
-        if self.shape == "flat":
-            return self.center - 0.6 * self.width
-        if self.shape == "custom":
-            return self._nu[self._pb > 1e-2 * self._pb.max()].min()
-
-        return self.center - 1.5 * self.width
-
-    @property
-    def nu_max(self) -> float:
-        if self.shape == "flat":
-            return self.center + 0.6 * self.width
-        if self.shape == "custom":
-            return self._nu[self._pb > 1e-2 * self._pb.max()].max()
-
-        return self.center + 1.5 * self.width
+        # dummy values for center and width
+        band = cls(nu=nu, tau=tau, shape="custom", **kwargs)
+        band.nu = nu
+        band.tau = tau
 
     def passband(self, nu):
-        """
-        Passband response as a function of nu (in GHz).
-        """
-        _nu = np.atleast_1d(nu)
-
-        if self.shape == "top_hat":
-            return np.exp(np.log(0.5) * (2 * (_nu - self.center) / self.width) ** 8)
-
-        if self.shape == "gaussian":
-            return np.exp(np.log(0.5) * (2 * (_nu - self.center) / self.width) ** 2)
-
-        if self.shape == "flat":
-            return np.where((_nu > self.nu_min) & (_nu < self.nu_max), 1.0, 0.0)
-
-        elif self.shape == "custom":
-            return np.interp(_nu, self._nu, self._pb)
+        return self.efficiency * sp.interpolate.interp1d(
+            self.nu, self.tau, bounds_error=False, fill_value=0
+        )(nu)
 
     @property
     def dP_dTRJ(self) -> float:
@@ -266,11 +297,12 @@ class Band:
         In watts per kelvin Rayleigh-Jeans, assuming perfect transmission.
         """
 
-        nu = np.linspace(self.nu_min, self.nu_max, 256)
+        # nu = np.linspace(self.nu_min, self.nu_max, 256)
 
         # dI_dTRJ = rayleigh_jeans_spectrum(nu=1e9 * nu, T=1)  # this is the same as the derivative
         # dP_dTRJ = np.trapezoid(dI_dTRJ * self.passband(nu), 1e9 * nu)
-        dP_dTRJ = k_B * np.trapezoid(self.passband(nu), 1e9 * nu)
+
+        dP_dTRJ = k_B * np.trapezoid(self.passband(self.nu), 1e9 * self.nu)
 
         return self.efficiency * dP_dTRJ
 
@@ -283,17 +315,16 @@ class Band:
         eps = 1e-3
         delta_T = np.array([-eps / 2, eps / 2])
 
-        nu = np.linspace(self.nu_min, self.nu_max, 256)
         TRJ = (
-            planck_spectrum(nu=1e9 * nu, T=T_CMB + delta_T[:, None])
+            planck_spectrum(nu=1e9 * self.nu, T=T_CMB + delta_T[:, None])
             * c**2
-            / (2 * k_B * (1e9 * nu) ** 2)
+            / (2 * k_B * (1e9 * self.nu) ** 2)
         )
 
         return (
             self.efficiency
             * k_B
-            * np.diff(np.trapezoid(TRJ * self.passband(nu), 1e9 * nu))[0]
+            * np.diff(np.trapezoid(TRJ * self.passband(self.nu), 1e9 * self.nu))[0]
             / eps
         )
 
@@ -378,11 +409,8 @@ class BandList(Sequence):
 
     def plot(self):
         for band in self.bands:
-            nus = np.linspace(band.nu_min, band.nu_max, 256)
-
             fig, ax = plt.subplots(1, 1)
-
-            ax.plot(nus, band.passband(nus), label=band.name)
+            ax.plot(band.nu, band.tau, label=band.name)
 
         ax.set_xlabel(r"$\nu$ [GHz]")
         ax.set_ylabel(r"$\tau(\nu)$ [Rayleigh-Jeans]")

maria/mappers/base.py
@@ -75,8 +75,8 @@ class BaseMapper:
         for band in self.bands:
             self.map_data[band] = self._run(band)
 
-        map_data = np.zeros((len(self.map_data), self.n_y, self.n_x))
-        map_weight = np.zeros((len(self.map_data), self.n_y, self.n_x))
+        map_data = np.zeros((len(self.map_data), 1, self.n_y, self.n_x))
+        map_weight = np.zeros((len(self.map_data), 1, self.n_y, self.n_x))
         map_freqs = []
 
         for i, (band_name, band_map_data) in enumerate(self.map_data.items()):
@@ -97,8 +97,8 @@ class BaseMapper:
                     size=self.map_postprocessing["median_filter"]["size"],
                 )
 
-            map_data[i] = band_map_numer / band_map_denom
-            map_weight[i] = band_map_denom
+            map_data[i, :] = band_map_numer / band_map_denom
+            map_weight[i, :] = band_map_denom
 
         return Map(
             data=map_data,

maria/plan/__init__.py
@@ -1,8 +1,7 @@
 import logging
 import os
-from dataclasses import dataclass, field
 from datetime import datetime, timedelta
-from typing import Tuple, Union
+from typing import Mapping, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -54,33 +53,75 @@ def get_plan(plan_name="one_minute_zenith_stare", **kwargs):
     return Plan(**plan_config)
 
 
-@dataclass
+PLAN_FIELDS = {
+    "start_time": Union[float, str, datetime],
+    "duration": float,
+    "sample_rate": float,
+    "frame": str,
+    "degrees": bool,
+    "scan_center": float,
+    "scan_pattern": float,
+    "scan_options": Mapping,
+}
+
+
+def validate_pointing_kwargs(kwargs):
+    """
+    Make sure that we have all the ingredients to produce the plan data.
+    """
+    if ("end_time" not in kwargs.keys()) and ("duration" not in kwargs.keys()):
+        raise ValueError(
+            """One of 'end_time' or 'duration' must be in the plan kwargs."""
+        )
+
+
 class Plan:
     """
     A dataclass containing time-ordered plan data.
     """
 
-    description: str = ""
-    start_time: Union[str, int] = "2022-02-10T06:00:00"
-    duration: float = 60.0
-    sample_rate: float = 20.0
-    frame: str = "ra_dec"
-    degrees: bool = True
-    scan_center: Tuple[float, float] = (4, 10.5)
-    scan_pattern: str = "daisy_miss_center"
-    scan_options: dict = field(default_factory=dict)
+    def __repr__(self):
+        parts = {}
+        frame = coords.frames[self.frame]
+        center_degrees = np.degrees(self.scan_center_radians)
 
-    @staticmethod
-    def validate_pointing_kwargs(kwargs):
-        """
-        Make sure that we have all the ingredients to produce the plan data.
-        """
-        if ("end_time" not in kwargs.keys()) and ("duration" not in kwargs.keys()):
-            raise ValueError(
-                """One of 'end_time' or 'duration' must be in the plan kwargs."""
-            )
+        parts["start_time"] = self.start_datetime.isoformat()[:19]
+        parts[
+            f"center[{frame['phi']}, {frame['theta']}]"
+        ] = f"{center_degrees[0]:.02f}°, {center_degrees[1]:.02f}°)"
+        parts["pattern"] = self.scan_pattern
+        parts["pattern_kwargs"] = self.scan_options
 
-    def __post_init__(self):
+        part_string = ""
+        for k, v in parts.items():
+            part_string += f"{k}={f'{v}' if isinstance(v, str) else v}"
+
+        return f"Plan({', '.join(parts)})"
+
+    def __init__(
+        self,
+        description: str = "",
+        start_time: Union[str, int] = "2024-02-10T06:00:00",
+        duration: float = 60.0,
+        sample_rate: float = 20.0,
+        frame: str = "ra_dec",
+        degrees: bool = True,
+        jitter: float = 0,
+        scan_center: Tuple[float, float] = (4, 10.5),
+        scan_pattern: str = "daisy",
+        scan_options: dict = {},
+    ):
+        self.description = description
+        self.start_time = start_time
+        self.duration = duration
+        self.sample_rate = sample_rate
+        self.frame = frame
+        self.degrees = degrees
+        self.jitter = jitter
+        self.scan_center = scan_center
+        self.scan_pattern = scan_pattern
+        self.scan_options = scan_options
+
         if not self.sample_rate > 0:
             raise ValueError("Parameter 'sample_rate' must be greater than zero!")
 
@@ -128,11 +169,6 @@ class Plan:
             x_scan_offsets_radians, y_scan_offsets_radians
         ].T
 
-        # add 0.1 arcseconds of jitter
-        self.scan_offsets_radians += np.radians(0.1 / 3600) * np.random.standard_normal(
-            size=self.scan_offsets_radians.shape
-        )
-
         scan_velocity_radians = np.gradient(
             self.scan_offsets_radians, axis=1, edge_order=0
         ) / np.gradient(self.time)
@@ -160,6 +196,11 @@ class Plan:
                 ),
                 stacklevel=2,
             )
+
+        # add 0.1 arcseconds of jitter
+        self.scan_offsets_radians += np.radians(
+            self.jitter
+        ) * np.random.standard_normal(size=self.scan_offsets_radians.shape)
 
         self.phi, self.theta = coords.dx_dy_to_phi_theta(
             *self.scan_offsets_radians, *self.scan_center_radians

maria/sim/map.py
@@ -37,24 +37,20 @@ class MapMixin:
 
             band_mask = self.instrument.dets.band_name == band.name
 
-            nu = np.linspace(band.nu_min, band.nu_max, 64)
+            nu_min = np.nanmin([band.nu.min(), self.map.nu.min()])
+            nu_max = np.nanmax([band.nu.max(), self.map.nu.max()])
+            nus = [nu_min, *(self.map.nu[1:] + self.map.nu[:-1]) / 2, nu_max]
 
-            TRJ = sp.interpolate.interp1d(
-                self.map.nu,
-                self.map.data,
-                axis=1,
-                kind="nearest",
-                bounds_error=False,
-                fill_value="extrapolate",
-            )(nu)
+            # a fast separable approximation to the band integral
+            power_map = 0
+            for nu1, nu2, nu_bin_TRJ in zip(nus[:-1], nus[1:], self.map.data):
+                nu = np.linspace(nu1, nu2, 1024)  # in GHz
+                tau = band.passband(nu)
 
-            power_map = (
-                1e12
-                * k_B
-                * np.trapezoid(
-                    band.passband(nu)[:, None, None] * TRJ, axis=1, x=1e9 * nu
-                )
-            )
+                # in pW
+                power_map += (
+                    1e12 * band.efficiency * k_B * np.trapezoid(tau, x=1e9 * nu)
+                ) * nu_bin_TRJ
 
             # nu is in GHz, f is in Hz
             nu_fwhm = beam.compute_angular_fwhm(
@@ -62,14 +58,16 @@ class MapMixin:
                 z=np.inf,
                 nu=band.center,
             )
+
             nu_map_filter = beam.construct_beam_filter(
                 fwhm=nu_fwhm, res=self.map.resolution
             )
+
             filtered_power_map = beam.separably_filter_2d(power_map, nu_map_filter)
 
-            if len(self.map.time) > 1:
+            if len(self.map.t) > 1:
                 map_power = sp.interpolate.RegularGridInterpolator(
-                    (self.map.time, self.map.x_side, self.map.y_side),
+                    (self.map.t, self.map.x_side, self.map.y_side),
                     filtered_power_map,
                     bounds_error=False,
                     fill_value=0,

maria/spectrum/__init__.py
@@ -43,8 +43,8 @@ class AtmosphericSpectrum:
                     f[key]["relative"][:] * f[key]["scale"][:] + f[key]["offset"][:],
                 )
 
-        self._transmission = np.exp(-self._opacity)
-        del self._opacity
+        # self._transmission = np.exp(-self._opacity)
+        # del self._opacity
 
     def emission(self, nu, pwv=None, base_temperature=None, elevation=45):
         if pwv is None:
@@ -62,7 +62,7 @@ class AtmosphericSpectrum:
             values=self._emission,
         )((pwv, base_temperature, elevation, nu))
 
-    def transmission(self, nu, pwv=None, base_temperature=None, elevation=45):
+    def opacity(self, nu, pwv=None, base_temperature=None, elevation=45):
         if pwv is None:
             pwv = np.median(self._side_zenith_pwv)
         if base_temperature is None:
@@ -75,5 +75,12 @@ class AtmosphericSpectrum:
                 self._side_elevation,
                 self._side_nu,
             ),
-            values=self._transmission,
+            values=self._opacity,
         )((pwv, base_temperature, elevation, nu))
+
+    def transmission(self, nu, pwv=None, base_temperature=None, elevation=45):
+        return np.exp(
+            -self.opacity(
+                nu, pwv=pwv, base_temperature=base_temperature, elevation=elevation
+            )
+        )