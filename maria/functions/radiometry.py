4b13b468cb45e15e164871c161c60c1eedd242c5
Thomas Morris <thomas.w.morris@yale.edu>
2024-11-27 15:21:32 -0500

    change calibration scheme
    Former-commit-id: 9d5942d0e38cfb6fff627544aff8fbc07d71f890 [formerly 6c22b623d2b43ea0af47fe3406684f9469bba0c2] [formerly 0bf6af753d833ec2018132200609f36de25bf394 [formerly 38d7ff1fe1f17c06c84d06cf1d91d093d9c9bb2b] [formerly 0bf6af753d833ec2018132200609f36de25bf394 [formerly 38d7ff1fe1f17c06c84d06cf1d91d093d9c9bb2b] [formerly 81c9d4a4689c7c53ed34cce8b4a637ff20b649b8]]]
    Former-commit-id: a1b2b9d506b1d980f9de6ea34972ebdaddcbef4f [formerly 0d81eada439b61d9299c36aaac554af2fdee7545]
    Former-commit-id: 9098045b91bb86f1ee77cb19241ff526132b079d
    Former-commit-id: 3b39066ec79f2577c0796af524ad766e0c5c5ebb [formerly 7f5820c7f20c140c2c106781e4c00a231173f5dd] [formerly c8de0ec4acf749ceeb18711385c9052486a10178]
    Former-commit-id: 97cd7d8864874133d9198eb168eaf6c11f57c8c5
    Former-commit-id: 5a7ebc5b839c4db941aa9cdb31355887b11131dc

---
+++ maria/functions/radiometry.py
@@ -0,0 +1,18 @@
+import numpy as np
+from ..constants import c, h, k_B  # noqa
+
+
+def rayleigh_jeans_spectrum(T_RJ: float, nu: float):
+    return 2 * k_B * nu**2 * T_RJ / c**2
+
+
+def inverse_rayleigh_jeans_spectrum(I_nu: float, nu: float):
+    return I_nu * c**2 / (2 * k_B * nu**2)
+
+
+def planck_spectrum(T_b: float, nu: float):
+    return 2 * h * nu**3 / (c**2 * np.expm1(h * nu / (k_B * T_b)))
+
+
+def inverse_planck_spectrum(I_nu: float, nu: float):
+    return (h * nu / k_B) / np.log1p(2 * h * nu**3 / (I_nu * c**2))
--- maria/tod/__init__.py
+++ maria/tod/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 from . import signal, tod  # noqa
 from .processing import process_tod  # noqa
 from .tod import TOD  # noqa
+from .field import Field  # noqa
 
 TOD.process = process_tod  # to avoid circular imports
