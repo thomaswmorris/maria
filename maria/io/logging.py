d7c58430c883b39d32964e466a6429dcc954884d
Thomas Morris <thomas.w.morris@yale.edu>
2025-01-18 19:22:17 -0500

    better transmission calibration
    Former-commit-id: e5d1fb8b6af87b3e4127ca0bff51e20b75da8d7b [formerly eecbe488f69cc9c426e8607890376085ec4bb231] [formerly fc8d7f955d0a296d30f02757709f003129a8a58a [formerly 5c8386d2aefa0593aaecca5e87b4254ef7c52236] [formerly fc8d7f955d0a296d30f02757709f003129a8a58a [formerly 5c8386d2aefa0593aaecca5e87b4254ef7c52236] [formerly 7a37fd8c0273493e37f1187a84861447fb5999fe]]]
    Former-commit-id: 5ab0d79be908ea6b81287af7505595380f89eb42 [formerly 5f91ebc41708650b4a0e6baac50b6c6a53c36d89]
    Former-commit-id: 3cf8d06052a9afa3f5323553d5871ac1db85fd5b
    Former-commit-id: 88cab2178835cf779d2c7460c8fecefca55a4f9e [formerly ab4b5200e1103c5550e4df17578888e8d5fcec00] [formerly 0b9bd3169e4761fd41789cd23429536ad5fe994b]
    Former-commit-id: 6e9021def2212680cfea238e0dd1663e2dbabb85
    Former-commit-id: 9b5b72095922c8db3285d0c1287ccb049a4b3e89

--- maria/io/logging.py
+++ maria/io/logging.py
@@ -3,22 +3,23 @@ import logging
 from ..units import prefixes
 
 
-def humanize_time(seconds):
-
+def humanize(x, unit=""):
     for _, prefix in prefixes.iterrows():
-        if prefix.time:
-            value = seconds / prefix.factor
+        if prefix.primary:
+            value = x / prefix.factor
             if value < 1e3:
                 break
-
     if value > 100:
         value = round(value)
     elif value > 10:
         value = round(value, 1)
     else:
         value = round(value, 2)
+    return f"{value} {prefix.name}{unit}"
 
-    return f"{value} {prefix.name}s"
+
+def humanize_time(seconds):
+    return humanize(seconds, unit="s")
 
 
 def log_duration(ref_time, message, level="debug"):
--- maria/units/si.py
+++ maria/units/si.py
@@ -7,6 +7,6 @@ import pandas as pd
 here, this_filename = os.path.split(__file__)
 
 prefixes = pd.read_csv(f"{here}/si-prefixes.csv", index_col=0)
-prefixes.loc[""] = "", "", 1e0
+prefixes.loc[""] = "", "", 0, 1e0
 prefixes.sort_values("factor", ascending=True, inplace=True)
-prefixes.loc[:, "time"] = np.log10(prefixes.factor.values) % 3 == 0
+prefixes.loc[:, "primary"] = np.log10(prefixes.factor.values) % 3 == 0
