fix noise units in instrument configs

maria/noise/__init__.py
@@ -21,11 +21,11 @@ def generate_noise_with_knee(
     else:
         noise = np.random.standard_normal(size=(n, len(t))).astype(np.float32)
 
-    noise *= NEP / np.sqrt(timestep)  # scale the noise
+    noise *= 1 / np.sqrt(timestep)  # scale the noise
 
     if knee > 0:
         f = np.fft.fftfreq(len(t), d=timestep)
-        a = knee * NEP**2 / 2
+        a = knee / 2
         with np.errstate(divide="ignore"):
             pink_noise_power_spectrum = np.where(f != 0, a / np.abs(f), 0)
 

maria/sim/simulation.py
@@ -189,21 +189,16 @@ class Simulation(BaseSimulation, AtmosphereMixin, CMBMixin, MapMixin, NoiseMixin
         if self.noise:
             self._simulate_noise()
 
-        # if hasattr(self, "atmospheric_transmission"):
-        #     for k in ["cmb", "map"]:
-        #         if k in self.data:
-        #             self.data[k] *= self.atmospheric_transmission
-
         gain_error = np.exp(
             self.instrument.dets.gain_error
             * np.random.standard_normal(size=self.instrument.dets.n),
         )
 
-        for field in self.data:
+        for field in self.loading:
             if field in ["noise"]:
                 continue
 
-            self.data[field] *= gain_error[:, None]
+            self.loading[field] *= gain_error[:, None]
 
     def __repr__(self):
         # object_reprs = [
@@ -212,3 +207,7 @@ class Simulation(BaseSimulation, AtmosphereMixin, CMBMixin, MapMixin, NoiseMixin
         return "\n".join(
             [self.instrument.__repr__(), self.site.__repr__(), self.plan.__repr__()],
         )
+
+    @property
+    def total_loading(self):
+        return sum([d for d in self.loading.values()])

maria/tests/test_noise.py
@@ -15,7 +15,7 @@ def test_noise_levels():
     )
     tod = sim.run()
 
-    target_error = sim.instrument.dets.NEP / np.sqrt(sim.plan.duration)
+    target_error = 1e12 * sim.instrument.dets.NEP / np.sqrt(sim.plan.duration)
 
     # this is should be distributed as a zero-mean unit-variance Gaussian
     scaled_residuals = tod.noise.compute().mean(axis=1) / target_error