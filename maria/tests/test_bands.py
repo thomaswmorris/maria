88cab2178835cf779d2c7460c8fecefca55a4f9e
Thomas Morris <thomas.w.morris@yale.edu>
2025-01-18 19:22:17 -0500

    better transmission calibration
    Former-commit-id: e5d1fb8b6af87b3e4127ca0bff51e20b75da8d7b [formerly eecbe488f69cc9c426e8607890376085ec4bb231] [formerly fc8d7f955d0a296d30f02757709f003129a8a58a [formerly 5c8386d2aefa0593aaecca5e87b4254ef7c52236] [formerly fc8d7f955d0a296d30f02757709f003129a8a58a [formerly 5c8386d2aefa0593aaecca5e87b4254ef7c52236] [formerly 7a37fd8c0273493e37f1187a84861447fb5999fe]]]
    Former-commit-id: 5ab0d79be908ea6b81287af7505595380f89eb42 [formerly 5f91ebc41708650b4a0e6baac50b6c6a53c36d89]
    Former-commit-id: 3cf8d06052a9afa3f5323553d5871ac1db85fd5b
    Former-commit-id: 0b9bd3169e4761fd41789cd23429536ad5fe994b
    Former-commit-id: ab4b5200e1103c5550e4df17578888e8d5fcec00

--- maria/tests/test_bands.py
+++ maria/tests/test_bands.py
@@ -3,19 +3,20 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from maria.instrument import Band
-
-
-def test_band_auto():
-    b = Band(center=130, width=10)
-    b.plot()
-    print(b.dP_dTRJ)
+from maria.band import Band, BandList, all_bands, get_band
 
 
 def test_band_manual():
     nu = np.linspace(120, 180, 64)
     tau = np.exp(-(((nu - 150) / (2 * 10)) ** 2))
-
-    b = Band(nu=nu, tau=tau, center=100)
+    b = Band(nu=nu, tau=tau)
     b.plot()
-    print(b.dP_dTRJ)
+
+
+@pytest.mark.parametrize("band_name", all_bands)
+def test_band_list(band_name):
+
+    bl = BandList()
+    for band_name in all_bands:
+        band = get_band(band_name)
+        bl.add(band)
