fix units in tests
Former-commit-id: 5230519bf261c35ecdeaa16515b35e67725de9ab [formerly 09b9aabf970d55cb53a40b55294a4f75df7bb3c3] [formerly 294b546cfb47f3d36bfd46667b062cd1330206ae [formerly 66b0da6efa5065d47527c98961f056e56bd896ba]]
Former-commit-id: 6d493e334690786c0067349c9710b5f9dacd3021 [formerly e5fc710421098415aacd5a3f2f3dec26e4f2a777]
Former-commit-id: 968fbaeb89631b86b46e1c9f2e83a17821564a27

maria/map/base.py
@@ -40,21 +40,12 @@ class Map:
 
         self.stokes = [param.upper() for param in stokes] if stokes is not None else ["I"]
 
-        # if nu is None:
-        #     logger.warning("No map frequency specified, assuming nu=150 GHz.")
-        #     self.nu = np.array([150.e9])
-        # elif nu < 1e6:
-        #     logger.warning("Assuming ")
-        #     self.nu = 1e9 * np.atleast_1d(nu)
-        # else:
-        #     self.nu = np.atleast_1d(nu)
+        self.nu = np.atleast_1d(nu if nu is not None else 150.0e9)
 
-        if np.min(nu) < MIN_NU:
+        if self.nu.min() < MIN_NU:
             raise ValueError(f"'nu' should be specified in Hz; maximum supported nu is {Quantity(MIN_NU, units='Hz')}.")
-        if np.max(nu) > MAX_NU:
+        if self.nu.min() > MAX_NU:
             raise ValueError(f"'nu' should be specified in Hz; maximum supported nu is {Quantity(MAX_NU, units='Hz')}.")
-
-        self.nu = np.atleast_1d(nu)
 
         self.t = np.atleast_1d(t) if t is not None else np.array([ttime.time()])
 

maria/tests/test_maps.py
@@ -20,7 +20,7 @@ def test_fetch_fits_map(map_name):
     map_filename = fetch(map_name)
     m = maria.map.load(
         filename=map_filename,
-        nu=90,
+        nu=90e9,
         resolution=1 / 1024,
         center=(150, 10),
         units="Jy/pixel",
@@ -37,7 +37,7 @@ def test_map_units_conversion(map_name):
     map_filename = fetch(map_name)
     m = maria.map.load(
         filename=map_filename,
-        nu=90,
+        nu=90e9,
         resolution=1 / 1024,
         center=(150, 10),
         units="Jy/pixel",
@@ -49,14 +49,14 @@ def test_map_units_conversion(map_name):
 @pytest.mark.parametrize("map_name", ["maps/sun.h5"])  # noqa
 def test_fetch_hdf_map(map_name):
     map_filename = fetch(map_name)
-    m = maria.map.load(filename=map_filename)
+    m = maria.map.load(filename=map_filename, nu=100e9)
     m.plot()
 
 
 def test_trivial_recover_original_map():
-    f090 = Band(center=90, width=30)
-    f150 = Band(center=150, width=30)
-    f220 = Band(center=220, width=30)
+    f090 = Band(center=90e9, width=30e9)
+    f150 = Band(center=150e9, width=30e9)
+    f220 = Band(center=220e9, width=30e9)
 
     array = {
         "field_of_view": 0.08333,
@@ -68,7 +68,9 @@ def test_trivial_recover_original_map():
     instrument = maria.get_instrument(array=array)
 
     map_filename = fetch("maps/big_cluster.fits", refresh=True)
-    input_map = maria.map.read_fits(filename=map_filename, nu=150, width=0.1, center=(150, 10)).downsample(n_x=100, n_y=100)
+    input_map = maria.map.read_fits(filename=map_filename, nu=150e9, width=0.1, center=(150, 10)).downsample(
+        n_x=100, n_y=100
+    )
 
     plan = maria.Plan(
         scan_pattern="daisy",
@@ -110,7 +112,7 @@ def test_trivial_recover_original_map():
 
 def test_time_ordered_map_sim():
     time_evolving_sun_path = fetch("maps/sun.h5")
-    input_map = maria.map.load(filename=time_evolving_sun_path, t=1.7e9 + np.linspace(0, 180, 16))
+    input_map = maria.map.load(filename=time_evolving_sun_path, nu=100e9, t=1.7e9 + np.linspace(0, 180, 16))
     plan = maria.Plan(
         start_time=1.7e9,
         duration=180,