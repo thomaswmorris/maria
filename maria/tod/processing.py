fix el gradient bug

maria/tod/processing.py
@@ -98,6 +98,8 @@ def process_tod(tod, config=None, **kwargs):
         D -= np.linspace(D[..., 0], D[..., -1], D.shape[-1]).T
 
         logger.debug(f'Completed tod operation "remove_slope in {humanize_time(ttime.monotonic() - remove_slope_start_s)}.')
+        if np.isnan(D).any():
+            raise ValueError("tod operation 'remove_slope' introduced NaNs")
 
     if "remove_spline" in config:
         remove_spline_start_s = ttime.monotonic()
@@ -108,9 +110,12 @@ def process_tod(tod, config=None, **kwargs):
             order=config["remove_spline"].get("order", 3),
         )
 
-        if config["remove_spline"].get("remove_el_gradient", True):
+        if config["remove_spline"].get("remove_el_gradient", False):
             el = tod.boresight.el.compute()
-            rel_el = (el - el.min()) / (el.max() - el.min())
+            el_ptp = el.max() - el.min()
+            if el_ptp == 0:
+                raise ValueError("Cannot remove elevation gradient when elevation is constant")
+            rel_el = (el - el.min()) / el_ptp
             B = np.concatenate([B * rel_el[None], B * (1 - rel_el)[None]], axis=0)
 
         A = np.linalg.inv(B @ B.T) @ B @ D.T
@@ -119,6 +124,8 @@ def process_tod(tod, config=None, **kwargs):
         logger.debug(
             f'Completed tod operation "remove_spline" in {humanize_time(ttime.monotonic() - remove_spline_start_s)}.'
         )
+        if np.isnan(D).any():
+            raise ValueError("tod operation 'remove_spline' introduced NaNs")
 
     if "window" in config:
         window_start_s = ttime.monotonic()
@@ -126,6 +133,8 @@ def process_tod(tod, config=None, **kwargs):
         W *= window_function(D.shape[-1], **config["window"].get("kwargs", {}))
         D *= W
         logger.debug(f'Completed tod operation "window" in {humanize_time(ttime.monotonic() - window_start_s)}.')
+        if np.isnan(D).any():
+            raise ValueError("tod operation 'window' introduced NaNs")
 
     if "filter" in config:
         filter_start_s = ttime.monotonic()
@@ -154,6 +163,8 @@ def process_tod(tod, config=None, **kwargs):
             )
 
         logger.debug(f'Completed tod operation "filter" in {humanize_time(ttime.monotonic() - filter_start_s)}.')
+        if np.isnan(D).any():
+            raise ValueError("tod operation 'filter' introduced NaNs")
 
     if "remove_modes" in config:
         remove_modes_start_s = ttime.monotonic()
@@ -163,6 +174,8 @@ def process_tod(tod, config=None, **kwargs):
         D -= A[:, modes_to_remove] @ B[modes_to_remove]
 
         logger.debug(f'Completed tod operation "remove_modes" in {humanize_time(ttime.monotonic() - remove_modes_start_s)}.')
+        if np.isnan(D).any():
+            raise ValueError("tod operation 'remove_modes' introduced NaNs")
 
     ptod = TOD(
         data={"total": D},