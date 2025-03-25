5eba10b1c1f592a0ce511cf264b9a13563e93522
thomaswmorris <thomaswmorris@users.noreply.github.com>
2025-03-25 02:31:13 +0000

    deploy: 3265a0389201f2632a2c3bde26d3ae0166c30cec [formerly 1306eb7e42ab40cf91743372742406458dd54652] [formerly 4ed75aed651335048531119136fdbe99f3bb964a] [formerly 3c697c4f7c5497ad803e22f189d42e78729c893a] [formerly ebfec5e2645f5214b9b94581a8cdd3b87b0ea71f [formerly 809317932e14199fc5e191070aa4cbd6a9d82f67]] [formerly 926da93d5af85a9c64b9ea0f74feb6c1b9817472]
    Former-commit-id: b4d8a9ec8096074fe241dc91c9afd950623b45bd
    Former-commit-id: 055cda80acbaf31abc5bb002c702f6c024a1bcb7
    Former-commit-id: c06254e40394455af852672069355f610d8573c2 [formerly 9a6bb963c900b4c60f79975584c4ae5079980be8] [formerly 00cb7f7f0ebd9590035490b94d1077a2600c31d0 [formerly b467e6c9b912416747ebbacccdaabb506639310e]]
    Former-commit-id: a6f6e6dbb7f0813d2c4d3d1b154b2773648445ea [formerly 3d7b9832f6084aa32e664e821bc9d3889cbad841]
    Former-commit-id: 78b10cbc0db1f1297d8df7d49b9d2dd682f85d58
    Former-commit-id: 62061e9420acae1d56cf7b43adbf4fbcd201ea5f
    Former-commit-id: 7d61725196414bb7d5639289ac5dcd5d190cb22c

--- _downloads/b57eadaf73f556292e23c7e4475d42a4/maps-1.py
+++ _downloads/b57eadaf73f556292e23c7e4475d42a4/maps-1.py
@@ -1,13 +1,12 @@
 import maria
 
-map_filename = maria.io.fetch("maps/big_cluster.fits")
+map_filename = maria.io.fetch("maps/big_cluster.h5")
 
-input_map = maria.map.read_fits(filename=map_filename,
-                                index=1, # which index of the HDU to read
-                                nu=150., # in GHz
-                                resolution=1/1024,
-                                center=(150, 10),
-                                frame="ra_dec",
-                                units="Jy/pixel")
+input_map = maria.map.load(filename=map_filename,
+                           nu=150e9, # in Hz
+                           resolution=1/1024,
+                           center=(150, 10),
+                           frame="ra_dec",
+                           units="Jy/pixel")
 
 input_map.to(units="uK_RJ").plot()
