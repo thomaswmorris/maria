16b28cd37d6b313aec6e6e17ec582dbb3d7324b0
Thomas Morris <thomas.w.morris@yale.edu>
2025-05-09 22:51:41 -0400

    change map path

--- maria/tests/test_simulation.py
+++ maria/tests/test_simulation.py
@@ -5,7 +5,9 @@ import os
 import numpy as np
 import pytest
 
+import maria
 from maria import Simulation, all_instruments, all_sites, get_plan
+from maria.mappers import BinMapper
 from maria.utils import read_yaml
 
 here, this_filename = os.path.split(__file__)
@@ -23,8 +25,19 @@ test_sites = np.random.choice(a=all_sites, size=n_sims)
     "instrument,site",
     zip(test_instruments, test_sites),
 )
-def test_complete_sim(instrument, site):
-    plan = get_plan("five_second_stare")
+def test_pipeline(instrument, site):
+    site = maria.get_site(site)
+
+    plan = maria.Plan(
+        start_time="2024-08-06T09:00:00",
+        scan_pattern="daisy",
+        scan_options={"radius": 0.5, "speed": 0.1},  # in degrees
+        duration=5,  # in seconds
+        sample_rate=20,  # in Hz
+        scan_center=(31, 62),
+        frame="az_el",
+    )
+
     sim = Simulation(
         instrument=instrument,
         site=site,
@@ -42,3 +55,25 @@ def test_complete_sim(instrument, site):
     tod = tod.to("K_RJ")
 
     tod.process(config={"remove_spline": {"knot_spacing": 60}})
+
+    mapper = BinMapper(
+        center=(0, -23),
+        stokes="IQUV",
+        frame="ra_dec",
+        width=1,
+        height=1,
+        resolution=1 / 256,
+        tod_preprocessing={
+            "window": {"name": "tukey", "kwargs": {"alpha": 0.1}},
+            "remove_spline": {"knot_spacing": 30, "remove_el_gradient": True},
+            "remove_modes": {"modes_to_remove": [0]},
+        },
+        map_postprocessing={
+            "gaussian_filter": {"sigma": 1},
+            "median_filter": {"size": 1},
+        },
+        units="mK_RJ",
+    )
+
+    mapper.add_tods(tod)
+    mapper.run()
