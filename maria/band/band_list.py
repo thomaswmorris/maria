8f831a0fc0c789a2ac4f00ee1c2cfa1be43d6f98
Thomas Morris <thomas.w.morris@yale.edu>
2025-05-09 23:14:37 -0400

    allow duplicate bands

--- maria/band/band_list.py
+++ maria/band/band_list.py
@@ -76,7 +76,8 @@ class BandList(Sequence):
         if not isinstance(band, Band):
             raise ValueError("'band' must be a Band type.")
         if band.name in self.names:
-            raise RuntimeError(f"There is already a band called '{band.name}'.")
+            if band not in self.bands:
+                raise RuntimeError(f"There is already a band called '{band.name}'.")
         self.bands.append(band)
 
     def __getattr__(self, attr):
