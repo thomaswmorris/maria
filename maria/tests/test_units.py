e74b2d5063404f3caf605a879c64c766fb98cc68
Thomas Morris <thomas.w.morris@yale.edu>
2025-05-15 15:56:01 -0400

    refactoring for jax
    Former-commit-id: bb0645c5eb7589587e498b8d54fbe60bf0e3a647
    Former-commit-id: e4b4be2d144fd5854ed153a26693b4b19fa8ddce

--- maria/tests/test_units.py
+++ maria/tests/test_units.py
@@ -7,7 +7,8 @@ from maria.units import Quantity
 
 
 def test_units():
-    assert str(Quantity(0.1, "deg")) == "6 arcmin"
+    assert str(Quantity(0.1, "deg")) == "6’"
+    assert str(Quantity(1e-8, "deg")) == "36 uarcsec"
     assert str(Quantity(1e6, "uK_RJ")) == "1 K_RJ"
     assert str(Quantity(1e15, "K_RJ")) == "1e+15 K_RJ"
     assert str(Quantity(1e6, "m")) == "1000 km"
