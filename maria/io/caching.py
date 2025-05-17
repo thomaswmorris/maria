bff2257945534f891ca6c0b432ee8f44bc040f06
Thomas Morris <thomas.w.morris@yale.edu>
2025-05-16 20:19:06 -0400

    make data caching more robust

--- maria/io/caching.py
+++ maria/io/caching.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import time as ttime
 
 import requests
@@ -13,6 +14,13 @@ here, this_filename = os.path.split(__file__)
 
 def set_cache_dir(directory):
     os.environ["MARIA_CACHE_DIR"] = directory
+
+
+def copy_file(source, destination):
+    dest_dir, _ = os.path.split(destination)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir, exist_ok=True)
+    shutil.copy(source, destination)
 
 
 def cache_status(path: str, max_age: float = 30 * 86400, refresh: bool = False):
@@ -34,8 +42,8 @@ def cache_status(path: str, max_age: float = 30 * 86400, refresh: bool = False):
     cache_age = ttime.time() - os.path.getmtime(path)
 
     if cache_age > max_age:
-        logger.debug(f"Cached file at {path} is too old.")
-        return "old"
+        logger.debug(f"Cached file at {path} is stale.")
+        return "stale"
 
     return "ok"
 
@@ -44,7 +52,7 @@ def download_from_url(
     source_url: str,
     cache_path: str = None,
     chunk_size: int = 2**12,
-    max_retries: int = 4,
+    max_age: int = 30 * 86400,
 ):
     """
     Download the cache if needed.
@@ -56,36 +64,27 @@ def download_from_url(
         logger.debug(f"Creating {cache_dir}")
         os.makedirs(cache_dir, exist_ok=True)
 
-    status = {"fails": 0, "success": False}
-    while not status["success"]:
-        try:
-            with requests.get(source_url, stream=True) as r:
-                r.raise_for_status()
-                total_size_bytes = int(r.headers.get("content-length", 0))
-                # logger.info(f"Caching data from {source_url}")
-                with tqdm(
-                    total=total_size_bytes,
-                    unit="B",
-                    unit_scale=True,
-                    desc=f"Downloading {source_url}",
-                ) as pbar:
-                    with open(cache_path, "wb") as f:
-                        for chunk in r.iter_content(chunk_size):
-                            f.write(chunk)
-                            pbar.update(len(chunk))
-            status["success"] = True
+    try:
+        with requests.get(source_url, stream=True) as r:
+            r.raise_for_status()
+            total_size_bytes = int(r.headers.get("content-length", 0))
+            # logger.info(f"Caching data from {source_url}")
+            with tqdm(
+                total=total_size_bytes,
+                unit="B",
+                unit_scale=True,
+                desc=f"Downloading {source_url}",
+            ) as pbar:
+                with open(cache_path, "wb") as f:
+                    for chunk in r.iter_content(chunk_size):
+                        f.write(chunk)
+                        pbar.update(len(chunk))
 
-        except Exception as error:
-            if status["fails"] < max_retries:
-                status["fails"] += 1
-                ttime.sleep(5e0)
-            else:
-                raise error
+    except Exception as error:
+        logger.warning(f"Encountered error while downloading {source_url}: {repr(error)}")
 
-        if not test_file(cache_path):
-            raise RuntimeError("Could not open cached file.")
-
-        return cache_path
+    finally:
+        return cache_status(cache_path, max_age=max_age, refresh=False)
 
 
 def fetch(
@@ -94,23 +93,43 @@ def fetch(
     max_age: float = 7 * 86400,
     refresh: bool = False,
     url_base: str = "https://github.com/thomaswmorris/maria-data/raw/master",
+    max_attempts: int = 7,
     **download_kwargs,
 ):
     """
     Fetch a file from the repo.
     """
-    cache_dir = os.environ.get("MARIA_CACHE_DIR", "/tmp/maria-data")
-    cache_path = cache_path or f"{cache_dir}/{source_path}"
-    url = f"{url_base}/{source_path}"
 
+    cache_dir = os.environ.get("MARIA_CACHE_DIR", f"/tmp/maria-data")
+    cache_path = cache_path or f"{cache_dir}/{source_path}"
+    source_url = f"{url_base}/{source_path}"
+
+    # do we need to do anything?
     status = cache_status(cache_path, max_age=max_age, refresh=refresh)
 
-    if status != "ok":
-        try:
-            download_from_url(url, cache_path=cache_path, **download_kwargs)
-        except Exception as error:
-            if status == "old":
-                logger.info(f"Encountered an error while trying to download {url}: {repr(error)}. Reverting to old cache.")
-            else:
-                raise RuntimeError(f"Encountered an error while trying to download {url}: {repr(error)}")
-    return cache_path
+    if status == "ok":
+        return cache_path
+
+    # do we have a potential backup?
+    if status == "stale":
+        stale_cache_path = f"{cache_dir}/stale/{source_path}"
+        copy_file(cache_path, stale_cache_path)
+    else:
+        stale_cache_path = None
+
+    attempt = 0
+    while attempt < max_attempts:
+        status = download_from_url(source_url, cache_path=cache_path, max_age=max_age, **download_kwargs)
+        if status == "ok":
+            return cache_path
+        if status == "missing":
+            raise FileNotFoundError(f"{source_path} does not exist")
+        attempt += 1
+        logger.warning(f"Could not download {source_url} on try {attempt} (status = {status})")
+        ttime.sleep(1e0)
+
+    if stale_cache_path:
+        logger.warning(f"Could not download {source_url}, using stale cache at {stale_cache_path}")
+        return stale_cache_path
+
+    raise RuntimeError(f"Could not download {source_url} after {max_attempts} retries (status = {status})")
