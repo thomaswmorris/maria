00c49af1a39f7b8641c76a3a8bb69bc85ac5d3e0
Thomas Morris <thomas.w.morris@yale.edu>
2025-06-06 13:30:43 -0400

    remove bad tests
    Former-commit-id: 8bb5e0f070dbeccaba647d8148e8a1c5e7b53777 [formerly 23f17671a506bd5c27b5b219f3a7bf0fb1c5abc2] [formerly 556a6cf785219ffafcd5ea0598320bbd1e9e5072]
    Former-commit-id: 5d1c746192b9433b4ade6fdff799858bca473e18
    Former-commit-id: d46f42fa46ec0787a878dde3fb4eb89198572b20
    Former-commit-id: ceeeb371adccbfaa6a8836c24487f3c188d77356
    Former-commit-id: 2446aece273e9967796e6be05fdbfcec52002bba

--- maria/tests/test_units.py
+++ maria/tests/test_units.py
@@ -10,6 +10,6 @@ def test_units():
     assert str(Quantity(0.1, "deg")) == "6’"
     assert str(Quantity(1e-8, "deg")) == "36 uarcsec"
     assert str(Quantity(1e6, "uK_RJ")) == "1 K_RJ"
-    assert str(Quantity(1e15, "K_RJ")) == "1e+15 K_RJ"
-    assert str(Quantity(1e6, "m")) == "1000 km"
+    assert str(Quantity(1e15, "K_RJ")) == "1e+15 K_RJ"  # don't go high
+    assert str(Quantity(1e-6, "Hz")) == "1e-06 Hz"  # don't go low
     assert str(Quantity(1000, "s")) == "1000 s"
