8f04b399bab61fe65d995d19ce9258f7787bc710
Thomas Morris <thomas.w.morris@yale.edu>
2025-07-10 12:12:24 -0400

    updated tod plot
    Former-commit-id: e08a4acbbff442b2377af2f6c7d16d283f11a89c [formerly 8fc596117f1e11e0a29af520c30f7dde134ae584] [formerly 3557460c1aec0796e488f8265ee2d7a15617699a]
    Former-commit-id: 6bf91e40d4877eb48ba2a40c73f5642c676ef600
    Former-commit-id: a3d7dfeaef5e269936dde0a50180bfccf0a5f6f8
    Former-commit-id: 898026904135f44ddc654e3c5d1efa342e71bfae
    Former-commit-id: ded072b8ef2b333ee66589eb24a16feebfa176a0

---
+++ maria/utils/coords.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def dms_to_deg(d: float = 0, m: float = 0, s: float = 0):
+    return np.radians(d + m / 60 + s / 3600)
+
+
+def hms_to_deg(h: float = 0, m: float = 0, s: float = 0):
+    return np.radians(h * 15 + m * 15 / 60 + s * 15 / 3600)
+
+
+def deg_to_signed_dms(x: float):
+    sign = -1 if x < 0 else 1
+    mnt, sec = divmod(abs(x) * 3600, 60)
+    deg, mnt = divmod(mnt, 60)
+    return int(sign), int(deg), int(mnt), sec
+
+
+def deg_to_signed_hms(x: float):
+    sign = -1 if x < 0 else 1
+    mnt, sec = divmod(abs(x) * 3600 / 15, 60)
+    deg, mnt = divmod(mnt, 60)
+    return int(sign), int(deg), int(mnt), sec
