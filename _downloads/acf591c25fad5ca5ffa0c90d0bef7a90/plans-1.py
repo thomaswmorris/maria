eb892c513a707a4459ed532a099bda5891d3fb40
thomaswmorris <thomaswmorris@users.noreply.github.com>
2025-08-12 13:22:21 +0000

    deploy: 02de743088aeb39cc57c2e8f9995aafea61c6f8b [formerly 89ea50b825b3d28688bab86717a9e0f678897ea9] [formerly 936b4b0b0eaffc23a2c7a5801ce62a92c4d4683a]
    Former-commit-id: ac613368b565e2330f0a114fe38d31368b2be439
    Former-commit-id: 7c2da4a94d716236f6a46134a5e6bf8fdc7608c5

--- _downloads/acf591c25fad5ca5ffa0c90d0bef7a90/plans-1.py
+++ _downloads/acf591c25fad5ca5ffa0c90d0bef7a90/plans-1.py
@@ -5,7 +5,9 @@ input_map = maria.map.load(maria.io.fetch("maps/crab_nebula.fits")) # load an ex
 
 planner = Planner(target=input_map,
                   site="green_bank",
-                  el_bounds=(60, 90)) # make a planner
+                  constraints={"el": (70, 90), # in degrees
+                               "min_sun_distance": 20, # in degrees
+                               "hour": (14, 15)})
 
 plan = planner.generate_plan(total_duration=600, # in seconds
                              scan_options={"radius": input_map.width.deg / 3}, # in degrees
