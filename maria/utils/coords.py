aa88e684c3bc824ec175c5951f465f22b5383b23
Thomas Morris <thomas.w.morris@yale.edu>
2025-08-24 14:55:22 -0400

    fix map plotting bug
    Former-commit-id: f7efe77cb767725cfb69030e056ed8f58fdac611
    Former-commit-id: 91ca9d68e60331f97cb703d020a1abe509f4534a

--- maria/utils/coords.py
+++ maria/utils/coords.py
@@ -9,14 +9,16 @@ def hms_to_deg(h: float = 0, m: float = 0, s: float = 0):
     return np.radians(h * 15 + m * 15 / 60 + s * 15 / 3600)
 
 
-def deg_to_signed_dms(x: float):
+def deg_to_signed_dms(x: float, precision: int = 6):
+    x = round(x, precision)
     sign = -1 if x < 0 else 1
     mnt, sec = divmod(abs(x) * 3600, 60)
     deg, mnt = divmod(mnt, 60)
     return int(sign), int(deg), int(mnt), sec
 
 
-def deg_to_signed_hms(x: float):
+def deg_to_signed_hms(x: float, precision: int = 6):
+    x = round(x, precision)
     sign = -1 if x < 0 else 1
     mnt, sec = divmod(abs(x) * 3600 / 15, 60)
     deg, mnt = divmod(mnt, 60)
