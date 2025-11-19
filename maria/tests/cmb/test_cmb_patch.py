f2a0453a3f9f2f6d09026bc7e35925cfbd5a6d1c
Thomas Morris <thomas.w.morris@yale.edu>
2025-11-19 16:31:10 -0500

    implemented cmb patch generation
    Former-commit-id: b540251de503403ec343a2c5bc0359c9b5a015ef
    Former-commit-id: 5ac797482ea47bb61f3d3d32c16497a8ce34521e

--- maria/tests/cmb/test_cmb_patch.py
+++ maria/tests/cmb/test_cmb_patch.py
@@ -4,16 +4,10 @@ import maria
 import pytest
 
 
-@pytest.mark.parametrize("nside", [256, 512, 1024, 2048])
-def test_generate_cmb(nside):
-    cmb = maria.cmb.generate_cmb(nside=nside)
-    # cmb.plot()
-
-
-def test_cmb_calibration():
+def test_cmb_patch():
     plan = maria.Plan.generate(
         scan_pattern="daisy",
-        scan_options={"radius": 10, "speed": 1},  # in degrees
+        scan_options={"radius": 5, "speed": 1},  # in degrees
         duration=120,  # in seconds
         sample_rate=50,  # in Hz
         scan_center=(150, 50),
@@ -21,18 +15,20 @@ def test_cmb_calibration():
         frame="az/el",
     )
 
+    cmb_patch = maria.cmb.generate_cmb_patch(width=15, center=plan.center())
+
     sim = maria.Simulation(
         instrument="test/1deg",
         plans=plan,
         site="cerro_toco",
-        cmb="generate",
+        map=cmb_patch,
         noise=False,
     )
 
-    tod = sim.run()[0].to("uK_CMB")
+    tod = sim.run()[0].to("K_CMB")
 
     # should be around 110 uK
-    cmb_rms_uK = tod.signal.std(axis=-1).compute()
+    cmb_rms_K = tod.signal.std(axis=-1).compute()
 
-    assert all(cmb_rms_uK > 80)
-    assert all(cmb_rms_uK < 150)
+    assert all(cmb_rms_K > 0.5 * cmb_patch.rms)
+    assert all(cmb_rms_K < 2.0 * cmb_patch.rms)
