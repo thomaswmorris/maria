6f78af148a362187849643b5e1902c5b03c42b3e
Aileen Chen <chena@odslserv01.t2.mpcdf.mpg.de>
2026-04-20 15:59:42 +0200

    jax sample maps in map.py

--- maria/sim/map.py
+++ maria/sim/map.py
@@ -8,9 +8,15 @@ import arrow
 import dask.array as da
 import numpy as np
 import scipy as sp
+import jax
+import jax.numpy as jnp
+import jax.lax as lax
 from jax import jit
 from jax import scipy as jsp
+from jax.experimental import sparse as jsparse
 from tqdm import tqdm
+from functools import partial
+import matplotlib.pyplot as plt
 
 from ..beam import compute_angular_fwhm
 from ..constants import k_B
@@ -21,6 +27,36 @@ from ..units import Quantity
 here, this_filename = os.path.split(__file__)
 logger = logging.getLogger("maria")
 
+@jax.jit(static_argnames=["band_coords_shape"])
+def apply_pointing(P, map_vec, pW_per_K_RJ, band_coords_shape):
+    return pW_per_K_RJ * (P @ map_vec).reshape(band_coords_shape)
+
+@jax.jit(static_argnames=["band_coords_shape"])
+def _sample_maps_jax(
+    map_loading,
+    band_coords_shape,
+    pW_per_K_RJ_all,
+    P_all,
+    map_vec_all,
+    band_indices
+):
+
+    def band_step(map_loading, inputs):
+        band_idx = inputs
+
+        for i in range(len(pW_per_K_RJ_all)):
+            pW_per_K_RJ = pW_per_K_RJ_all[i]
+            P = P_all[i]
+            map_vec = map_vec_all[i]
+
+            pW = apply_pointing(P, map_vec, pW_per_K_RJ, band_coords_shape)
+            map_loading = map_loading.at[band_idx].add(pW)
+
+        return map_loading, None
+
+    map_loading, _ = lax.scan(band_step, map_loading, band_indices)
+
+    return map_loading
 
 DEFAULT_MAP_SIM_KWARGS = {
     "bilinear_sampling": True,
@@ -72,39 +108,50 @@ class MapMixin:
         self._sample_maps(**kwargs)
 
     def _sample_maps(self, obs):
-        map_loading = np.zeros(obs.coords.shape, dtype=self.dtype)
+        map_loading = jnp.zeros(obs.coords.shape, dtype=self.dtype)
+        instrument = obs.instrument
+        disable_progress_bars = self.disable_progress_bars
+        coords = obs.coords
+        map = self.map
+        atmosphere = obs.atmosphere
 
         bands_pbar = tqdm(
-            obs.instrument.dets.bands,
+            instrument.dets.bands,
             desc="Sampling map",
-            disable=self.disable_progress_bars,
+            disable=disable_progress_bars,
             bar_format=DEFAULT_BAR_FORMAT,
             ncols=250,
             postfix={"band": "", "channel": "", "stokes": ""},
         )
-        for band in bands_pbar:
-            bands_pbar.set_postfix(band=band.name)
 
-            band_mask = obs.instrument.dets.band_name == band.name
-            band_coords = obs.coords[band_mask]
-            band_dets = obs.instrument.dets[band_mask]
+        # store values for jax computation
+        pW_per_K_RJ_all = []
+        P_all = []
+        map_vec_all = []
+        band_index_list = []
+
+        for band in bands_pbar:
+            band_mask = instrument.dets.band_name == band.name
+            indices = np.where(band_mask)[0]
+            band_index_list.append(indices)
+
+            band_dets = instrument.dets[band_mask]
+            band_coords = coords[band_mask]
 
             band_fwhm = Quantity(
                 compute_angular_fwhm(
-                    fwhm_0=obs.instrument.dets.primary_size.mean(),
+                    fwhm_0=instrument.dets.primary_size.mean(),
                     z=np.inf,
                     nu=band.center.Hz,
                 ),
                 "rad",
             )
 
-            # ideally we would do this for each nu bin, but that's slow
-            smoothed_map = self.map.smooth(fwhm=band_fwhm)
-
+            # input for forward model is smoothed map and not unsmoothed
+            smoothed_map = map.smooth(fwhm=band_fwhm)
             logger.debug(f"Convolved map with beam width {band_fwhm} for band {band.name}")
 
-            for channel_index, (nu_min, nu_max) in enumerate(self.map.nu_bin_bounds):
-                channel_s = ttime.monotonic()
+            for channel_index, (nu_min, nu_max) in enumerate(map.nu_bin_bounds):
                 channel_map = smoothed_map.to("K_RJ", band=band)[:, [channel_index]]
                 qchannel = (nu_min, nu_max)
                 channel_string = f"{qchannel}"
@@ -114,26 +161,25 @@ class MapMixin:
 
                 spectrum_kwargs = (
                     {
-                        "spectrum": obs.atmosphere.spectrum,
+                        "spectrum": atmosphere.spectrum,
                         "zenith_pwv": obs.zenith_scaled_pwv[band_mask].compute(),
-                        "base_temperature": obs.atmosphere.weather.temperature[0],
-                        "elevation": obs.coords.el[band_mask],
+                        "base_temperature": atmosphere.weather.temperature[0],
+                        "elevation": coords.el[band_mask],
                     }
                     if getattr(obs, "atmosphere", None)
                     else {}
                 )
-
                 calibration_s = ttime.monotonic()
-                pW_per_K_RJ = (
+                pW_per_K_RJ = jnp.array(
                     1e12
                     * k_B
                     * band.compute_transmission_integral(nu_min_Hz=nu_min.Hz, nu_max_Hz=nu_max.Hz, **spectrum_kwargs)
                 )
+                pW_per_K_RJ_all.append(pW_per_K_RJ)
                 logger.debug(
                     f"Computed K_RJ -> pW calibration for band {band.name}, channel {channel_string} in "
                     f"{humanize_time(ttime.monotonic() - calibration_s)}"
                 )
-
                 bands_pbar.set_postfix(band=band.name, channel=channel_string)
 
                 pointing_s = ttime.monotonic()
@@ -143,24 +189,14 @@ class MapMixin:
                     dets=band_dets,
                     bilinear=self.map_kwargs.get("bilinear_sampling", DEFAULT_MAP_SIM_KWARGS["bilinear_sampling"]),
                 )
+                P = jsparse.BCSR.from_scipy_sparse(P)
                 logger.debug(
                     f"Computed pointing matrix for band {band.name} in {humanize_time(ttime.monotonic() - pointing_s)}"
                 )
+                P_all.append(P)
+                map_vec_all.append(jnp.asarray(channel_map.data.ravel()))
 
-                pW = pW_per_K_RJ * (P @ channel_map.data.ravel()).reshape(band_coords.shape)
-
-                map_loading[band_mask] += pW
-
-                if logger.level == 10:
-                    logger.debug(
-                        f"Computed map load {pW.shape} ~{Quantity(pW.std(), 'pW'):<8} for band {band.name}, "
-                        f"channel {channel_string} in {humanize_time(ttime.monotonic() - channel_s)}"
-                    )
-
-                del pW
-
-            if map_loading[band_mask].sum() == 0:
-                logger.warning(f"No load from map for band {band.name}")
-
+        band_indices = jnp.asarray(band_index_list)
+        map_loading = _sample_maps_jax(map_loading, band_coords.shape, pW_per_K_RJ_all, P_all, map_vec_all, band_indices)
         obs.loading["map"] = da.asarray(map_loading, dtype=self.dtype)
         del map_loading
