bd53bc50f5ea71e2ef19a6ffec42f537884639e6
Thomas Morris <thomas.w.morris@yale.edu>
2026-05-15 09:48:22 -0400

    improve maps and tods
    Former-commit-id: 02fc6621bdaf60ee8247f4efa0cc1aa8520af856 [formerly b21f3fe5774bd85f9f5a49fb25c18961376fb635] [formerly 9d79c808dfdebaad3989c562615d5c372e2c373d]
    Former-commit-id: 7ab35d7a4274a0a8ca74e1f39d4890e95c6e8d4d
    Former-commit-id: 5ccda2c8c4b91e9a12b7d8660d2de75b4d7c98fc
    Former-commit-id: 9ada0bec57f74271d996785d245ff200490c8e3c
    Former-commit-id: 45916bfc9f7675e2b2240c176a40f6668268f983

--- maria/tod/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from . import signal, tod  # noqa
-from .processing import process_tod  # noqa
-from .tod import TOD, TOD_DIMENSIONS  # noqa
-
-TOD.process = process_tod  # to avoid circular imports
