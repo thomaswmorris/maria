42ee43a65465b958c5fa1e2aa4419fd7e88470fe
Thomas Morris <thomas.w.morris@yale.edu>
2026-05-20 23:57:21 -0400

    add residual maps
    Former-commit-id: 9fa00a99a363a21cd4c761f0b2075eaa749fd918 [formerly b9317797e306542806a8aca3abe079182f16a396] [formerly 4be9d7f3acc14586a593b9142cdaf8c5eb1497bb]
    Former-commit-id: b2a6cec6dec548ffb76a2c7921ae99c478aa3ab3
    Former-commit-id: 8f3744b3a1024161ed9216679bf519b4815bdee7
    Former-commit-id: 275b6b6c9a4a01862a284ee65ef61ca4049f8b67
    Former-commit-id: 276c5e22d9047cb5af5672f6522a9d1e43ab4529

--- maria/mappers/__init__.py
+++ maria/mappers/__init__.py
@@ -1,4 +1,19 @@
 from __future__ import annotations
 
+from ..map import Map
 from .bin_mapper import BinMapper  # noqa
 from .ml_mapper import MaximumLikelihoodMapper  # noqa
+
+
+def compute_residual_map(input_map: Map, output_map: Map):
+
+    in_map = input_map.copy()
+
+    for dim in output_map.dims:
+        if dim not in in_map.dims:
+            in_map.unsqueeze(dim)
+
+    residual_map = output_map.resample(in_map).to(in_map.units)
+    residual_map.data -= in_map.data
+
+    return residual_map
